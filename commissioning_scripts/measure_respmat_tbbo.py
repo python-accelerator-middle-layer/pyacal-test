f84bf5c57aee18238021fb45dd0b6f7ed6534ed8
murilobalves <alvesb.murilo@gmail.com>
2019-10-03 10:09:21 -0300

    ENH: Adapt scripts to use devices module

--- commissioning_scripts/measure_respmat_tbbo.py
+++ commissioning_scripts/measure_respmat_tbbo.py
@@ -8,57 +8,8 @@ from siriuspy.epics import PV
 from siriuspy.namesys import SiriusPVName as _PVName
 from siriuspy.csdevice.orbitcorr import SOFBFactory
 
+from apsuite.devices import SOFB
 from apsuite.optimization import SimulAnneal
-
-
-class SOFB:
-
-    def __init__(self, acc):
-        self.data = SOFBFactory.create(acc)
-        orbtp = 'MTurnIdx' if acc == 'BO' else 'SPass'
-        self._trajx = PV(acc+'-Glob:AP-SOFB:'+orbtp+'OrbX-Mon')
-        self._trajy = PV(acc+'-Glob:AP-SOFB:'+orbtp+'OrbY-Mon')
-        self._rst = PV(acc+'-Glob:AP-SOFB:SmoothReset-Cmd')
-        self._npts_sp = PV(acc+'-Glob:AP-SOFB:SmoothNrPts-SP')
-        self._npts_rb = PV(acc+'-Glob:AP-SOFB:BufferCount-Mon')
-
-    @property
-    def connected(self):
-        conn = self._trajx.connected
-        conn &= self._trajx.connected
-        conn &= self._rst.connected
-        conn &= self._npts_sp.connected
-        conn &= self._npts_rb.connected
-        return conn
-
-    @property
-    def trajx(self):
-        return self._trajx.get()
-
-    @property
-    def trajy(self):
-        return self._trajy.get()
-
-    @property
-    def nr_points(self):
-        return self._npts_rb.value
-
-    @nr_points.setter
-    def nr_points(self, value):
-        self._npts_sp.value = int(value)
-
-    def wait(self, timeout=10):
-        inter = 0.05
-        n = int(timeout/inter)
-        for _ in range(n):
-            if self._npts_rb.value >= self._npts_sp.value:
-                break
-            _time.sleep(inter)
-        else:
-            print('WARN: Timed out waiting orbit.')
-
-    def reset(self):
-        self._rst.value = 1
 
 
 class Params:
