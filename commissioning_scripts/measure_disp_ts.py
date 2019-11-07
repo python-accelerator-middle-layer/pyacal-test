39219e188d18765f769d844dff663d52ed5e3d81
murilobalves <alvesb.murilo@gmail.com>
2019-11-07 10:12:51 -0300

    COMMIS.ENH: Add prints and BO orbit meas

--- commissioning_scripts/measure_disp_ts.py
+++ commissioning_scripts/measure_disp_ts.py
@@ -17,17 +17,29 @@ class ParamsDisp:
     def __init__(self):
         """."""
         self.energy_delta = 0.005  # in GeV
-        self.wait_time = 40
+        self.wait_time = 2
         self.timeout_orb = 10
         self.num_points = 10
-        self.delay2energy = 0.4923/489994.304  # [GeV/us]
+        self.delay2energy = 0.4923/48994.304  # [GeV/us]
+        self.wait_update_events = 2.0  # [s]
 
     @property
     def ejection_delta(self):
         return self.energy_delta / self.delay2energy  # in us
 
+    def __str__(self):
+        """."""
+        strt = ''
+        strt += 'energy_delta: ' + str(self.energy_delta) + ' GeV \n'
+        strt += 'wait_time: ' + str(self.wait_time) + ' s\n'
+        strt += 'timeout_orb: ' + str(self.timeout_orb) + ' s\n'
+        strt += 'num_points: ' + str(self.num_points) + '\n'
+        strt += 'delay2energy: ' + str(self.delay2energy) + ' GeV/us\n'
+        strt += 'wait_update_events: ' + str(self.wait_update_events) + ' s\n'
+        return strt
 
-class MeasureDispTBBO(BaseClass):
+
+class MeasureDispTS(BaseClass):
     """."""
 
     HARMONIC_NUM = 828
@@ -37,6 +49,7 @@ class MeasureDispTBBO(BaseClass):
         super().__init__(ParamsDisp())
         self.devices = {
             'ts_sofb': SOFB('TS'),
+            'bo_sofb': SOFB('BO'),
             'rf': RF()
             }
         self.pvs = {
@@ -50,7 +63,7 @@ class MeasureDispTBBO(BaseClass):
     @property
     def energy(self):
         """."""
-        return self.params.delay2energy * self.pvs['injsi'].value
+        return self.params.delay2energy * self.injsi
 
     @property
     def trajx(self):
@@ -58,12 +71,23 @@ class MeasureDispTBBO(BaseClass):
         return self.devices['ts_sofb'].trajx
 
     @property
+    def trajx_bo(self):
+        """."""
+        return self.devices['bo_sofb'].trajx_idx
+
+    @property
     def trajy(self):
         """."""
         return self.devices['ts_sofb'].trajy
 
     @property
+    def trajy_bo(self):
+        """."""
+        return self.devices['bo_sofb'].trajy_idx
+
+    @property
     def injsi(self):
+        """."""
         return self.pvs['injsi_rb'].value
 
     @injsi.setter
@@ -72,13 +96,16 @@ class MeasureDispTBBO(BaseClass):
 
     @property
     def digts(self):
+        """."""
         return self.pvs['digts_rb'].value
 
     @digts.setter
     def digts(self, value):
+        """."""
         self.pvs['digts_sp'].value = value
 
     def update_events(self):
+        """."""
         self.pvs['update_evt'].value = 1
 
     @property
@@ -89,50 +116,71 @@ class MeasureDispTBBO(BaseClass):
     @nr_points.setter
     def nr_points(self, value):
         self.devices['ts_sofb'].nr_points = int(value)
+        self.devices['bo_sofb'].nr_points = int(value)
 
     def wait(self, timeout=10):
         """."""
         self.devices['ts_sofb'].wait(timeout=timeout)
+        self.devices['bo_sofb'].wait(timeout=timeout)
 
     def reset(self, wait=0):
         """."""
         _time.sleep(wait)
         self.devices['ts_sofb'].reset()
+        self.devices['bo_sofb'].reset()
         _time.sleep(1)
 
     def calc_delta(self, delta):
+        """."""
         # delta and revolution time in [us]
         t0 = self.HARMONIC_NUM/self.devices['rf'].frequency * 1e6
         return round(delta/t0)*t0
 
     def measure_dispersion(self):
         """."""
+        print('calc delta time')
         self.nr_points = self.params.num_points
         delta = self.calc_delta(delta=self.params.ejection_delta)
 
+        print('reset sofb')
         self.reset(self.params.wait_time)
         self.wait(self.params.timeout_orb)
+
+        print('read orbit')
         orb = [-np.hstack([self.trajx, self.trajy]), ]
+        orb_bo = [-np.hstack([self.trajx_bo, self.trajy_bo]), ]
         ene0 = self.energy
 
+        print('set delays')
         orig_delay_injsi = self.injsi
         orig_delay_digts = self.digts
         self.injsi = orig_delay_injsi + delta
         self.digts = orig_delay_digts + delta
+        _time.sleep(self.params.wait_update_events)
 
+        print('update events')
         self.update_events()
+        _time.sleep(self.params.wait_update_events)
+
+        print('reset sofb')
         self.reset(self.params.wait_time)
         self.wait(self.params.timeout_orb)
 
+        print('read orbit')
         orb.append(np.hstack([self.trajx, self.trajy]))
+        orb_bo.append(np.hstack([self.trajx_bo, self.trajy_bo]))
         ene1 = self.energy
 
+        print('restore original delays')
         self.injsi = orig_delay_injsi
         self.digts = orig_delay_digts
+        _time.sleep(self.params.wait_update_events)
         self.update_events()
+        _time.sleep(self.params.wait_update_events)
 
         d_ene = ene1/ene0 - 1
-        return np.array(orb).sum(axis=0) / d_ene
+        return np.array(orb).sum(axis=0) / d_ene, \
+               np.array(orb_bo).sum(axis=0) / d_ene
 
 
 def calc_model_dispersionTS(model, bpms):
