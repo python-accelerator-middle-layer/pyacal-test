3f69f9dde4cf966ae0cc0a02a3a26798e81dc470
murilobalves <alvesb.murilo@gmail.com>
2019-12-09 17:22:04 -0300

    RF.ENH: Add timing to control injection pulses in control RF script and change in DCCT device handling

--- commissioning_scripts/control_rf.py
+++ commissioning_scripts/control_rf.py
@@ -4,7 +4,7 @@
 import time as _time
 import numpy as np
 
-from pymodels.middlelayer.devices import DCCT, RF, SOFB
+from pymodels.middlelayer.devices import RF, SOFB, DCCT, Timing
 from apsuite.commissioning_scripts.base import BaseClass
 
 
@@ -21,11 +21,9 @@ class Params:
         self.voltage_delta = 1
         self.nrpulses = 20
         self.freq_pulses = 2
-        self.dcct_nrsamples = 50
-        self.dcct_period = 0.05
         self.sofb_timeout = 10
-        self.dcct_timeout = 10
         self.rf_timeout = 10
+        self.tim_timeout = 10
         self.wait_rf = 2
 
     def __str__(self):
@@ -42,13 +40,10 @@ class Params:
         st += '{0:30s}= {1:9d}\n'.format('number of pulses', self.nrpulses)
         st += '{0:30s}= {1:9.3f}\n'.format(
             'pulses freq [Hz]', self.freq_pulses)
-        st += '{0:30s}= {1:9d}\n'.format(
-            'DCCT number of samples', self.dcct_nrsamples)
-        st += '{0:30s}= {1:9.3f}\n'.format('DCCT period', self.dcct_period)
         st += '{0:30s}= {1:9.3f}\n'.format('SOFB timeout', self.sofb_timeout)
-        st += '{0:30s}= {1:9.3f}\n'.format('DCCT timeout', self.dcct_timeout)
         st += '{0:30s}= {1:9.3f}\n'.format('RF timeout', self.rf_timeout)
         st += '{0:30s}= {1:9.3f}\n'.format('Wait RF', self.wait_rf)
+        st += '{0:30s}= {1:9.3f}\n'.format('Timing timeout', self.tim_timeout)
         return st
 
 
@@ -63,7 +58,7 @@ class ControlRF(BaseClass):
         else:
             raise Exception('Set BO or SI')
         self.devices = {
-            'dcct': DCCT(),
+            'tim': Timing(),
             'rf': RF(acc=acc, is_cw=is_cw),
             'sofb': SOFB(acc=acc),
             }
@@ -71,11 +66,20 @@ class ControlRF(BaseClass):
             'phase': [],
             'voltage': [],
             'power': [],
-            'dcct': [],
             'sum': [],
             'orbx': [],
             'orby': [],
             }
+        if acc == 'BO':
+            self.devices['dcct'] = DCCT('BO')
+            self.data['dcct'] = []
+        elif acc == 'SI':
+            self.devices['dcct-1'] = DCCT('SI-1')
+            self.devices['dcct-2'] = DCCT('SI-2')
+            self.data['dcct-1'] = []
+            self.data['dcct-2'] = []
+
+
 
     @property
     def phase_span(self):
@@ -111,53 +115,66 @@ class ControlRF(BaseClass):
         freq = self.params.freq_pulses
 
         self.devices['sofb'].nr_points = nrpul
-        print('Turning DCCT Off')
-        self.devices['dcct'].turn_off(self.params.dcct_timeout)
-        print('Setting DCCT params')
-        self.devices['dcct'].nrsamples = self.params.dcct_nrsamples
-        self.devices['dcct'].period = self.params.dcct_period
-        _time.sleep(2)
-        print('Turning DCCT On')
-        self.devices['dcct'].turn_on(self.params.dcct_timeout)
 
         var_span = self.phase_span if isphase else self.voltage_span
         self.data['phase'] = []
         self.data['voltage'] = []
         self.data['power'] = []
-        self.data['dcct'] = []
         self.data['sum'] = []
         self.data['orbx'] = []
         self.data['orby'] = []
+        if self.acc == 'BO':
+            self.data['dcct'] = []
+        elif self.acc == 'SI':
+            self.data['dcct-1'] = []
+            self.data['dcct-2'] = []
         print('Starting Loop')
         for val in var_span:
+            print('Turning pulses off --> ', end='')
+            self.devices['tim'].turn_pulses_off(self.params.tim_timeout)
+            print('varying phase --> ', end='')
             self._vary(val, isphase=isphase)
             _time.sleep(self.params.wait_rf)
-            dcct_data = np.zeros(nrpul)
             phase_data = np.zeros(nrpul)
             voltage_data = np.zeros(nrpul)
             power_data = np.zeros(nrpul)
+            if self.acc == 'BO':
+                dcct_data = np.zeros(nrpul)
+            elif self.acc == 'SI':
+                dcct1_data = np.zeros(nrpul)
+                dcct2_data = np.zeros(nrpul)
+            print('turning pulses on --> ', end='')
+            self.devices['tim'].turn_pulses_on(self.params.tim_timeout)
+            print('Getting data ', end='')
             self.devices['sofb'].reset()
             for k in range(nrpul):
-                dcct_data[k] = np.mean(self.devices['dcct'].current)
+                print('.', end='')
                 phase_data[k] = self.devices['rf'].phase
                 voltage_data[k] = self.devices['rf'].voltage
                 power_data[k] = self.devices['rf'].power
+                if self.acc == 'BO':
+                    dcct_data[k] = np.mean(self.devices['dcct'].current)
+                elif self.acc == 'SI':
+                    dcct1_data[k] = np.mean(self.devices['dcct-1'].current)
+                    dcct2_data[k] = np.mean(self.devices['dcct-2'].current)
                 _time.sleep(1/freq)
             self.devices['sofb'].wait(self.params.sofb_timeout)
             self.data['phase'].append(phase_data)
             self.data['voltage'].append(voltage_data)
             self.data['power'].append(power_data)
-            self.data['dcct'].append(dcct_data)
             self.data['sum'].append(self.devices['sofb'].sum)
             self.data['orbx'].append(self.devices['sofb'].trajx)
             self.data['orby'].append(self.devices['sofb'].trajy)
+            if self.acc == 'BO':
+                self.data['dcct'].append(dcct_data)
+            elif self.acc == 'SI':
+                self.data['dcct-1'].append(dcct1_data)
+                self.data['dcct-2'].append(dcct2_data)
             if isphase:
-                print('Phase [°]: {0:8.3f} -> Current [uA]: {1:8.3f}'.format(
-                    self.devices['rf'].phase, np.mean(dcct_data)*1e3))
+                print('Phase [°]: {0:8.3f}'.format(self.devices['rf'].phase))
             else:
-                print(
-                    'Voltage [mV]: {0:8.3f} -> Current [uA]: {1:8.3f}'.format(
-                        self.devices['rf'].voltage, np.mean(dcct_data)*1e3))
+                print('Voltage [mV]: {0:8.3f}'.format(
+                    self.devices['rf'].voltage))
         print('Finished!')
 
     def _vary(self, val, isphase=True):
