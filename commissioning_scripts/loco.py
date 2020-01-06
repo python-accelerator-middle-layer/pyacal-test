1665c882b6b3211ca1adf3e904f17190c609cfc2
murilobalves <alvesb.murilo@gmail.com>
2020-01-06 17:12:16 -0300

    API: Add option to use quadrupoles families in fitting

--- commissioning_scripts/loco.py
+++ commissioning_scripts/loco.py
@@ -10,12 +10,21 @@ from apsuite.commissioning_scripts.calc_orbcorr_mat import Respmat
 
 class FitQuads():
 
-    def __init__(self, model):
+    QUAD_FAM = [
+        'QFA', 'QDA', 'QDB2', 'QFB', 'QDB1', 'QDP2', 'QFP', 'QDP1',
+        'Q1', 'Q2', 'Q3', 'Q4']
+
+    def __init__(self, model, dim='6d', use_families=False):
         self.model = model
-        self.respm = Respmat(model=model, dim='6d')
+        self.dim = dim
+        self.respm = Respmat(model=model, dim=dim)
         self.matrix = self.respm.get_respm()
         self.vector = self.matrix.flatten()
-        self.quadsidx = self.respm.fam_data['QN']['index']
+        self.use_families = use_families
+        self.quadsidx = []
+        for fam_name in self.QUAD_FAM:
+            self.quadsidx.append(self.respm.fam_data[fam_name]['index'])
+        self.kquads = []
         # self.kmatrix = self._calc_kmatrix()
 
     def _vector2respm(self, vector):
@@ -24,29 +33,43 @@ class FitQuads():
         return np.reshape(vector, (row, col))
 
     def _calc_kmatrix(self, deltak=1e-6):
-        self.kquads = pyaccel.lattice.get_attribute(
-            self.model, 'K', self.quadsidx)
-
-        nquads = len(self.kquads)
+        for qi in self.quadsidx:
+            self.kquads.append(pyaccel.lattice.get_attribute(
+                self.model, 'K', qi))
+        if self.use_families:
+            nquads = len(self.kquads)
+        else:
+            nquads = len([q for sub in self.kquads for q in sub])
         kmatrix = np.zeros((len(self.vector), nquads))
 
-        for i, idx in enumerate(self.quadsidx):
-            for ii, idx_seg in enumerate(idx):
-                pyaccel.lattice.set_attribute(
-                    self.model, 'K', idx_seg, self.kquads[i][ii] +
-                    deltak)
-            new_respm = Respmat(model=self.model, dim='6d').get_respm()
-            dmdk = (new_respm - self.matrix)/deltak
-            kmatrix[:, i] = dmdk.flatten()
-            for ii, idx_seg in enumerate(idx):
-                pyaccel.lattice.set_attribute(
-                    self.model, 'K', idx_seg, self.kquads[i][ii])
+        count = 0
+        for i1, fam in enumerate(self.quadsidx):
+            for i2, idx in enumerate(fam):
+                for i3, idx_seg in enumerate(idx):
+                    pyaccel.lattice.set_attribute(
+                        self.model, 'K', idx_seg, self.kquads[i1][i2][i3] +
+                        deltak)
+                if not self.use_families:
+                    new_respm = Respmat(model=self.model, dim='6d').get_respm()
+                    dmdk = (new_respm - self.matrix)/deltak
+                    kmatrix[:, count] = dmdk.flatten()
+                    count += 1
+
+            if self.use_families:
+                new_respm = Respmat(model=self.model, dim='6d').get_respm()
+                dmdk = (new_respm - self.matrix)/deltak
+                kmatrix[:, i1] = dmdk.flatten()
+
+            for i2, idx in enumerate(fam):
+                for i3, idx_seg in enumerate(idx):
+                    pyaccel.lattice.set_attribute(
+                        self.model, 'K', idx_seg, self.kquads[i1][i2][i3])
         return kmatrix
 
     def chi2(self, M1, M2):
         return np.sqrt(np.mean((M1-M2)**2))
 
-    def fit_matrices(self, model=None, measmat=None, niter=10, nsv=None):
+    def fit_matrices(self, model=None, measmat=None, kmatrix=None, niter=10,                nsv=None):
         if model is None:
             model = self.model
             modelmat = self.matrix
@@ -58,9 +81,12 @@ class FitQuads():
 
         diffmat = measmat - modelmat
         chi2_old = self.chi2(measmat, modelmat)
-        quadidx = self.respm.fam_data['QN']['index']
+        qnidx = self.respm.fam_data['QN']['index']
         print('Initial Matrix Deviation: {:.16e}'.format(chi2_old))
-        u, s, v = np.linalg.svd(self.kmatrix, full_matrices=False)
+        is_fam = False
+        if kmatrix.shape[1] < len(qnidx):
+            is_fam = True
+        u, s, v = np.linalg.svd(kmatrix, full_matrices=False)
         inv_s = 1/s
         inv_s[np.isnan(inv_s)] = 0
         inv_s[np.isinf(inv_s)] = 0
@@ -69,18 +95,34 @@ class FitQuads():
         inv_s = np.diag(inv_s)
         inv_kmatrix = np.dot(np.dot(v.T, inv_s), u.T)
 
-        grad_quads = np.array(
-            pyaccel.lattice.get_attribute(mod, 'K', quadidx))
+        if is_fam:
+            grad_quads = []
+            for qi in self.quadsidx:
+                grad_quads.append(pyaccel.lattice.get_attribute(
+                    self.model, 'K', qi))
+        else:
+            grad_quads = np.array(
+                pyaccel.lattice.get_attribute(mod, 'K', qnidx))
+
         grad_delta = np.zeros(inv_kmatrix.shape[0])
         tol = 1e-16
 
         for n in range(niter):
             grad_delta += np.dot(inv_kmatrix, diffmat.flatten())
 
-            for i, idx in enumerate(quadidx):
-                for ii, idx_seg in enumerate(idx):
-                    pyaccel.lattice.set_attribute(
-                        mod, 'K', idx_seg, grad_quads[i][ii] + grad_delta[i])
+            if is_fam:
+                for i1, fam in enumerate(self.quadsidx):
+                    for i2, idx in enumerate(fam):
+                        for i3, idx_seg in enumerate(idx):
+                            pyaccel.lattice.set_attribute(
+                                mod, 'K', idx_seg, grad_quads[i1][i2][i3] +
+                                grad_delta[i1])
+            else:
+                for i1, idx in enumerate(qnidx):
+                    for i2, idx_seg in enumerate(idx):
+                        pyaccel.lattice.set_attribute(
+                            mod, 'K', idx_seg, grad_quads[i1][i2] +
+                            grad_delta[i1])
 
             fitmat = Respmat(model=mod, dim='6d').get_respm()
             diffmat = measmat - fitmat
@@ -88,18 +130,29 @@ class FitQuads():
             chi2_new = self.chi2(measmat, fitmat)
             print('Matrix Deviation: {:.16e}'.format(chi2_new))
 
+            if np.isnan(chi2_new):
+                print('Penalty Function is NaN!')
+                break
+
             if (chi2_old - chi2_new) < tol:
                 print('Limit Reached!')
                 grad_delta -= np.dot(inv_kmatrix, diffmat.flatten())
 
-                for i, idx in enumerate(quadidx):
-                    for ii, idx_seg in enumerate(idx):
-                        pyaccel.lattice.set_attribute(
-                            mod, 'K', idx_seg,
-                            grad_quads[i][ii] + grad_delta[i])
+                if is_fam:
+                    for i1, fam in enumerate(self.quadsidx):
+                        for i2, idx in enumerate(fam):
+                            for i3, idx_seg in enumerate(idx):
+                                pyaccel.lattice.set_attribute(
+                                    mod, 'K', idx_seg, grad_quads[i1][i2][i3] +
+                                    grad_delta[i1])
+                else:
+                    for i1, idx in enumerate(qnidx):
+                        for i2, idx_seg in enumerate(idx):
+                            pyaccel.lattice.set_attribute(
+                                mod, 'K', idx_seg, grad_quads[i1][i2] +
+                                grad_delta[i1])
                 fitmat = Respmat(model=mod, dim='6d').get_respm()
                 break
             else:
                 chi2_old = chi2_new
-        fit_grad_quads = grad_quads[:, 0] + grad_delta
-        return fitmat, fit_grad_quads, mod
+        return fitmat, mod
