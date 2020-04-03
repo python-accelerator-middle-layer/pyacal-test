f3919535e2b9007d66d20dac24d3b618ad46b7ba
Fernando <fernandohds564@gmail.com>
2020-04-03 09:57:36 -0300

    FUNC.API: remove method polyfit.

--- functions.py
+++ functions.py
@@ -3,19 +3,6 @@ from functools import partial as _partial
 import numpy as _np
 
 
-def polyfit(x, y, monomials):
-    """Implement Custom polyfit."""
-    coef = _np.polynomial.polynomial.polyfit(x, y, deg=monomials)
-
-    # finds maximum diff and its base value
-    y_fitted = _np.polynomial.polynomial.polyval(x, coef)
-    y_diff = abs(y_fitted - y)
-    idx = _np.argmax(y_diff)
-
-    coeffs = coef[monomials]
-    return (coeffs, (y_diff[idx], y[idx]))
-
-
 def generate_random_numbers(n_part, dist_type='exp', cutoff=3):
     """Generate random numbers with a cutted off dist_type distribution.
 
