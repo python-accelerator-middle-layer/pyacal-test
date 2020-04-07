6a53f511032bf25aeb9aac71288a1c55f11b03ad
murilobalves <alvesb.murilo@gmail.com>
2020-04-07 10:29:26 -0300

    POSANG.STY: Following pylint recommendations

--- commissioning_scripts/calc_orbcorr_mat.py
+++ commissioning_scripts/calc_orbcorr_mat.py
@@ -20,8 +20,8 @@ class OrbRespmat():
             raise Exception('Set models: BO or SI')
         self.dim = dim
         self.bpms = self._get_idx(self.fam_data['BPM']['index'])
-        self.ch = self._get_idx(self.fam_data['CH']['index'])
-        self.cv = self._get_idx(self.fam_data['CV']['index'])
+        self.chidx = self._get_idx(self.fam_data['CH']['index'])
+        self.cvidx = self._get_idx(self.fam_data['CV']['index'])
 
     @staticmethod
     def _get_idx(indcs):
@@ -32,28 +32,28 @@ class OrbRespmat():
             self.model, 'pass_method', 'cavity_pass')[0]
         rffreq = self.model[idx].frequency
         if self.dim == '6d':
-            df = 10
-            self.model[idx].frequency = rffreq + df
+            dfreq = 10
+            self.model[idx].frequency = rffreq + dfreq
             orbp = pyaccel.tracking.find_orbit6(self.model, indices='open')
-            self.model[idx].frequency = rffreq - df
+            self.model[idx].frequency = rffreq - dfreq
             orbn = pyaccel.tracking.find_orbit6(self.model, indices='open')
             self.model[idx].frequency = rffreq
-            rfline = (orbp[[0, 2], :] - orbn[[0, 2], :])/2/df
+            rfline = (orbp[[0, 2], :] - orbn[[0, 2], :])/2/dfreq
             rfline = rfline[:, self.bpms].flatten()
         else:
-            de = 1e-5
+            denergy = 1e-5
             orbp = pyaccel.tracking.find_orbit4(
-                self.model, energy_offset=de, indices='open')
+                self.model, energy_offset=denergy, indices='open')
             orbn = pyaccel.tracking.find_orbit4(
-                self.model, energy_offset=-de, indices='open')
-            dispbpm = (orbp[[0, 2], :] - orbn[[0, 2], :])/2/de
+                self.model, energy_offset=-denergy, indices='open')
+            dispbpm = (orbp[[0, 2], :] - orbn[[0, 2], :])/2/denergy
             dispbpm = dispbpm[:, self.bpms].flatten()
 
             rin = np.zeros((6, 2))
-            rin[4, :] = [de, -de]
+            rin[4, :] = [denergy, -denergy]
             rout, *_ = pyaccel.tracking.ring_pass(self.model, rin)
             leng = self.model.length
-            alpha = -1 * np.diff(rout[5, :]) / 2 / de / leng
+            alpha = -1 * np.diff(rout[5, :]) / 2 / denergy / leng
             # Convert dispersion to deltax/deltaf:
             rfline = - dispbpm / rffreq / alpha
         return rfline
@@ -63,23 +63,25 @@ class OrbRespmat():
         cav = self.model.cavity_on
         self.model.cavity_on = self.dim == '6d'
         if self.dim == '6d':
-            M, T = pyaccel.tracking.find_m66(self.model, indices='open')
+            m_mat, t_mat = pyaccel.tracking.find_m66(
+                self.model, indices='open')
         else:
-            M, T = pyaccel.tracking.find_m44(self.model, indices='open')
+            m_mat, t_mat = pyaccel.tracking.find_m44(
+                self.model, indices='open')
 
-        nch = len(self.ch)
+        nch = len(self.chidx)
         respmat = []
-        corrs = np.hstack([self.ch, self.cv])
-        for i, corr in enumerate(corrs):
-            Rc = T[corr, :, :]
-            Rb = T[self.bpms, :, :]
+        corrs = np.hstack([self.chidx, self.cvidx])
+        for idx, corr in enumerate(corrs):
+            rc_mat = t_mat[corr, :, :]
+            rb_mat = t_mat[self.bpms, :, :]
             corr_len = self.model[corr].length
-            KL = self.model[corr].KL
-            KsL = self.model[corr].KsL
+            kl_stren = self.model[corr].KL
+            ksl_stren = self.model[corr].KsL
             respx, respy = self._get_respmat_line(
-                Rc, Rb, M, corr, corr_len,
-                kxl=KL, kyl=-KL, ksxl=KsL, ksyl=KsL)
-            if i < nch:
+                rc_mat, rb_mat, m_mat, corr, corr_len,
+                kxl=kl_stren, kyl=-kl_stren, ksxl=ksl_stren, ksyl=ksl_stren)
+            if idx < nch:
                 respmat.append(respx)
             else:
                 respmat.append(respy)
@@ -91,52 +93,53 @@ class OrbRespmat():
         self.model.cavity_on = cav
         return respmat
 
-    def _get_respmat_line(self, Rc, Rb, M, corr, length,
+    def _get_respmat_line(self, rc_mat, rb_mat, m_mat, corr, length,
                           kxl=0, kyl=0, ksxl=0, ksyl=0):
         # create a symplectic integrator of second order
         # for the last half of the element:
-        drift = np.eye(Rc.shape[0], dtype=float)
+        drift = np.eye(rc_mat.shape[0], dtype=float)
         drift[0, 1] = length/2 / 2
         drift[2, 3] = length/2 / 2
-        quad = np.eye(Rc.shape[0], dtype=float)
+        quad = np.eye(rc_mat.shape[0], dtype=float)
         quad[1, 0] = -kxl/2
         quad[3, 2] = -kyl/2
         quad[1, 2] = -ksxl/2
         quad[3, 0] = -ksyl/2
         half_cor = drift @ quad @ drift
-        Rc = half_cor @ Rc
+        rc_mat = half_cor @ rc_mat
 
-        Mc = np.linalg.solve(Rc.T, (Rc @ M).T).T  # Mc = Rc M Rc^-1
-        Mci = np.eye(Mc.shape[0], dtype=float) - Mc
+        mc_mat = np.linalg.solve(
+            rc_mat.T, (rc_mat @ m_mat).T).T  # Mc = Rc M Rc^-1
+        mci_mat = np.eye(mc_mat.shape[0], dtype=float) - mc_mat
 
         small = self.bpms < corr
         large = np.logical_not(small)
 
-        RcbL = np.linalg.solve(Rc.T, Rb.transpose((0, 2, 1)))
-        RcbL = RcbL.transpose((0, 2, 1))
-        RcbS = RcbL[small] @ Mc
-        RcbL = RcbL[large]
+        rcbl_mat = np.linalg.solve(rc_mat.T, rb_mat.transpose((0, 2, 1)))
+        rcbl_mat = rcbl_mat.transpose((0, 2, 1))
+        rcbs_mat = rcbl_mat[small] @ mc_mat
+        rcbl_mat = rcbl_mat[large]
 
-        RcbL = np.linalg.solve(Mci.T, RcbL.transpose((0, 2, 1)))
-        RcbL = RcbL.transpose((0, 2, 1))
-        RcbS = np.linalg.solve(Mci.T, RcbS.transpose((0, 2, 1)))
-        RcbS = RcbS.transpose((0, 2, 1))
+        rcbl_mat = np.linalg.solve(mci_mat.T, rcbl_mat.transpose((0, 2, 1)))
+        rcbl_mat = rcbl_mat.transpose((0, 2, 1))
+        rcbs_mat = np.linalg.solve(mci_mat.T, rcbs_mat.transpose((0, 2, 1)))
+        rcbs_mat = rcbs_mat.transpose((0, 2, 1))
 
         respxx = np.zeros(len(self.bpms))
         respyx = np.zeros(len(self.bpms))
         respxy = np.zeros(len(self.bpms))
         respyy = np.zeros(len(self.bpms))
 
-        respxx[large] = RcbL[:, 0, 1]
-        respyx[large] = RcbL[:, 2, 1]
-        respxx[small] = RcbS[:, 0, 1]
-        respyx[small] = RcbS[:, 2, 1]
+        respxx[large] = rcbl_mat[:, 0, 1]
+        respyx[large] = rcbl_mat[:, 2, 1]
+        respxx[small] = rcbs_mat[:, 0, 1]
+        respyx[small] = rcbs_mat[:, 2, 1]
         respx = np.hstack([respxx, respyx])
 
-        respxy[large] = RcbL[:, 0, 3]
-        respyy[large] = RcbL[:, 2, 3]
-        respxy[small] = RcbS[:, 0, 3]
-        respyy[small] = RcbS[:, 2, 3]
+        respxy[large] = rcbl_mat[:, 0, 3]
+        respyy[large] = rcbl_mat[:, 2, 3]
+        respxy[small] = rcbs_mat[:, 0, 3]
+        respyy[small] = rcbs_mat[:, 2, 3]
         respy = np.hstack([respxy, respyy])
         return respx, respy
 
@@ -158,17 +161,17 @@ class TrajRespmat():
             self.fam_data = si.get_family_data(model)
 
         self.bpms = self._get_idx(self.fam_data['BPM']['index'])
-        self.ch = self.fam_data['CH']['index']
+        self.chidx = self.fam_data['CH']['index']
 
         if acc == 'TS':
             ejesept = pyaccel.lattice.find_indices(
                 model, 'fam_name', 'EjeSeptG')
             segs = len(ejesept)
-            self.ch.append([ejesept[segs//2]])
-            self.ch = sorted(self.ch)
+            self.chidx.append([ejesept[segs//2]])
+            self.chidx = sorted(self.chidx)
 
-        self.ch = self._get_idx(self.ch)
-        self.cv = self._get_idx(self.fam_data['CV']['index'])
+        self.chidx = self._get_idx(self.chidx)
+        self.cvidx = self._get_idx(self.fam_data['CV']['index'])
 
     @staticmethod
     def _get_idx(indcs):
@@ -180,17 +183,17 @@ class TrajRespmat():
             self.model, indices='open', closed_orbit=[0, 0, 0, 0])
 
         respmat = []
-        corrs = np.hstack([self.ch, self.cv])
+        corrs = np.hstack([self.chidx, self.cvidx])
         for idx, corr in enumerate(corrs):
-            Rc = cumulmat[corr]
-            Rb = cumulmat[self.bpms]
+            rc_mat = cumulmat[corr]
+            rb_mat = cumulmat[self.bpms]
             corr_len = self.model[corr].length
-            KL = self.model[corr].KL
-            KsL = self.model[corr].KsL
+            kl_stren = self.model[corr].KL
+            ksl_stren = self.model[corr].KsL
             respx, respy = self._get_respmat_line(
-                Rc, Rb, corr, length=corr_len,
-                kxl=KL, kyl=-KL, ksxl=KsL, ksyl=KsL)
-            if idx < len(self.ch):
+                rc_mat, rb_mat, corr, length=corr_len,
+                kxl=kl_stren, kyl=-kl_stren, ksxl=ksl_stren, ksyl=ksl_stren)
+            if idx < len(self.chidx):
                 respmat.append(respx)
             else:
                 respmat.append(respy)
@@ -199,7 +202,7 @@ class TrajRespmat():
         respmat = np.array(respmat).T
         return respmat
 
-    def _get_respmat_line(self, Rc, Rb, corr, length,
+    def _get_respmat_line(self, rc_mat, rb_mat, corr, length,
                           kxl=0, kyl=0, ksxl=0, ksyl=0):
         # create a symplectic integrator of second order
         # for the last half of the element:
@@ -213,24 +216,24 @@ class TrajRespmat():
         quad[3, 0] = -ksyl/2
         half_cor = drift @ quad @ drift
 
-        Rc = half_cor @ Rc
+        rc_mat = half_cor @ rc_mat
 
         large = self.bpms > corr
 
-        Rb = Rb[large, :, :]
-        Rcb = np.linalg.solve(Rc.T, Rb.transpose((0, 2, 1)))
-        Rcb = Rcb.transpose(0, 2, 1)
+        rb_mat = rb_mat[large, :, :]
+        rcb_mat = np.linalg.solve(rc_mat.T, rb_mat.transpose((0, 2, 1)))
+        rcb_mat = rcb_mat.transpose(0, 2, 1)
 
         respxx = np.zeros(len(self.bpms), dtype=float)
         respyx = np.zeros(len(self.bpms), dtype=float)
         respxy = np.zeros(len(self.bpms), dtype=float)
         respyy = np.zeros(len(self.bpms), dtype=float)
 
-        respxx[large] = Rcb[:, 0, 1]
-        respyx[large] = Rcb[:, 2, 1]
+        respxx[large] = rcb_mat[:, 0, 1]
+        respyx[large] = rcb_mat[:, 2, 1]
         respx = np.hstack([respxx, respyx])
 
-        respxy[large] = Rcb[:, 0, 3]
-        respyy[large] = Rcb[:, 2, 3]
+        respxy[large] = rcb_mat[:, 0, 3]
+        respyy[large] = rcb_mat[:, 2, 3]
         respy = np.hstack([respxy, respyy])
         return respx, respy
--- commissioning_scripts/calc_posang_mat.py
+++ commissioning_scripts/calc_posang_mat.py
@@ -13,10 +13,10 @@ def get_tb_posang_respm(model=None):
         model, _ = tb.create_accelerator()
     fam_data = tb.families.get_family_data(model)
 
-    ch = [fam_data['CH']['index'][-1], fam_data['InjSept']['index'][0]]
-    cv = [fam_data['CV']['index'][-2], fam_data['CV']['index'][-1]]
+    chidx = [fam_data['CH']['index'][-1], fam_data['InjSept']['index'][0]]
+    cvidx = [fam_data['CV']['index'][-2], fam_data['CV']['index'][-1]]
 
-    return _calc_posang_matrices(model, ch, cv)
+    return _calc_posang_matrices(model, chidx, cvidx)
 
 
 def get_ts_posang_respm(corrs_type='CH-Sept', model=None):
@@ -26,48 +26,48 @@ def get_ts_posang_respm(corrs_type='CH-Sept', model=None):
     fam_data = ts.families.get_family_data(model)
 
     if corrs_type == 'CH-Sept':
-        ch = [fam_data['CH']['index'][-1],
-              fam_data['InjSeptF']['index'][0]]
+        chidx = [fam_data['CH']['index'][-1],
+                 fam_data['InjSeptF']['index'][0]]
     else:
-        ch = [fam_data['InjSeptG']['index'][0],
-              fam_data['InjSeptG']['index'][1],
-              fam_data['InjSeptF']['index'][0]]
-    cv = [fam_data['CV']['index'][-2], fam_data['CV']['index'][-1]]
+        chidx = [fam_data['InjSeptG']['index'][0],
+                 fam_data['InjSeptG']['index'][1],
+                 fam_data['InjSeptF']['index'][0]]
+    cvidx = [fam_data['CV']['index'][-2], fam_data['CV']['index'][-1]]
 
-    return _calc_posang_matrices(model, ch, cv)
+    return _calc_posang_matrices(model, chidx, cvidx)
 
 
-def _calc_posang_matrices(model, ch, cv):
-    ini = ch[0][0]
-    mat_h_aux = np.zeros((2, len(ch)))
-    for i in range(len(ch)):
+def _calc_posang_matrices(model, chidx, cvidx):
+    ini = chidx[0][0]
+    mat_h_aux = np.zeros((2, len(chidx)))
+    for idx in range(len(chidx)):
         pyaccel.lattice.set_attribute(
-            model, 'hkick_polynom', ch[i], KICK/2/len(ch[i]))
-        p1 = pyaccel.tracking.line_pass(model[ini:len(model)], 6*[0])
+            model, 'hkick_polynom', chidx[idx], KICK/2/len(chidx[idx]))
+        pos1, *_ = pyaccel.tracking.line_pass(model[ini:len(model)], 6*[0])
         pyaccel.lattice.set_attribute(
-            model, 'hkick_polynom', ch[i], -KICK/2/len(ch[i]))
-        p2 = pyaccel.tracking.line_pass(model[ini:len(model)], 6*[0])
+            model, 'hkick_polynom', chidx[idx], -KICK/2/len(chidx[idx]))
+        pos2, *_ = pyaccel.tracking.line_pass(model[ini:len(model)], 6*[0])
         pyaccel.lattice.set_attribute(
-            model, 'hkick_polynom', ch[i], 0)
-        mat_h_aux[:, i] = (p1[0][0:2] - p2[0][0:2])/KICK
+            model, 'hkick_polynom', chidx[idx], 0)
+        mat_h_aux[:, idx] = (pos1[0:2] - pos2[0:2])/KICK
 
-    if len(ch) == 3:
+    if len(chidx) == 3:
         mat_h = [[mat_h_aux[0, 0]+mat_h_aux[0, 1], mat_h_aux[0, 2]],
                  [mat_h_aux[1, 0]+mat_h_aux[1, 1], mat_h_aux[1, 2]]]
     else:
         mat_h = mat_h_aux
 
-    ini = cv[0][0]
+    ini = cvidx[0][0]
     mat_v = np.zeros((2, 2))
-    for i in range(len(cv)):
+    for idx in range(len(cvidx)):
         pyaccel.lattice.set_attribute(
-            model, 'vkick_polynom', cv[i], KICK/2/len(cv[i]))
-        p1 = pyaccel.tracking.line_pass(model[ini:len(model)], 6*[0])
+            model, 'vkick_polynom', cvidx[idx], KICK/2/len(cvidx[idx]))
+        pos1, *_ = pyaccel.tracking.line_pass(model[ini:len(model)], 6*[0])
         pyaccel.lattice.set_attribute(
-            model, 'vkick_polynom', cv[i], -KICK/2/len(cv[i]))
-        p2 = pyaccel.tracking.line_pass(model[ini:len(model)], 6*[0])
+            model, 'vkick_polynom', cvidx[idx], -KICK/2/len(cvidx[idx]))
+        pos2, *_ = pyaccel.tracking.line_pass(model[ini:len(model)], 6*[0])
         pyaccel.lattice.set_attribute(
-            model, 'vkick_polynom', cv[i], 0)
-        mat_v[:, i] = (p1[0][2:4] - p2[0][2:4])/KICK
+            model, 'vkick_polynom', cvidx[idx], 0)
+        mat_v[:, idx] = (pos1[2:4] - pos2[2:4])/KICK
 
     return mat_h, mat_v
