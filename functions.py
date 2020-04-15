e2c99de7ad9d9c56ab81e1c52ed065d0c46af780
Fernando <fernandohds564@gmail.com>
2020-04-15 11:19:53 -0300

    FUN.ENH: add method get_namedtuple, previously located at siriuspy.

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 """Useful functions."""
+from collections import namedtuple as _namedtuple
 from functools import partial as _partial
 import numpy as _np
 
@@ -33,3 +34,21 @@ def generate_random_numbers(n_part, dist_type='exp', cutoff=3):
         numbers -= 1/2
         numbers *= 2
     return numbers
+
+
+def get_namedtuple(name, field_names, values=None):
+    """Return an instance of a namedtuple Class.
+
+    Inputs:
+        - name:  Defines the name of the Class (str).
+        - field_names:  Defines the field names of the Class (iterable).
+        - values (optional): Defines field values . If not given, the value of
+            each field will be its index in 'field_names' (iterable).
+
+    Raises ValueError if at least one of the field names are invalid.
+    Raises TypeError when len(values) != len(field_names)
+    """
+    if values is None:
+        values = range(len(field_names))
+    field_names = [f.replace(' ', '_') for f in field_names]
+    return _namedtuple(name, field_names)(*values)
