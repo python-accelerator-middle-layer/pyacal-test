ad511d3807a17b3b8b5e3d8d967f6304925110e4
murilobalves <alvesb.murilo@gmail.com>
2020-07-07 13:28:58 -0300

    ID.ENH: Add analysis methods to fit kicks from meas. orbit distortion

--- commissioning_scripts/measure_id_integrals.py
+++ commissioning_scripts/measure_id_integrals.py
@@ -5,6 +5,7 @@ import numpy as _np
 from siriuspy.devices import SOFB, APU, Tune
 
 from epics import PV
+import pyaccel
 from pymodels import si
 from .base import BaseClass
 
@@ -52,6 +53,8 @@ class MeasIDIntegral(BaseClass):
         self.famdata = si.get_family_data(model)
         self.params = IDParams(phases, meas_type)
         self.id_name = id_name
+        self.id_idx = _np.array(self.famdata[self.id_name]['index']).flatten()
+        self.bpm_idx = _np.array(self.famdata['BPM']['index']).flatten()
         self.devices['apu'] = APU(self.id_name)
         self.devices['tune'] = Tune(Tune.DEVICES.SI)
         self.devices['sofb'] = SOFB(SOFB.DEVICES.SI)
@@ -127,6 +130,7 @@ class MeasIDIntegral(BaseClass):
         """."""
         self.devices['study_event'].value = 1
 
+# measurement
     def _meas_integral_static(self):
         ph_spd = self.params.phase_speed
         # sending to initial phase
@@ -204,3 +208,109 @@ class MeasIDIntegral(BaseClass):
         meas['timestamp'] = tstamp
         self.data['measure'] = meas
         print('finished!')
+
+# analysis
+    def _add_id_correctors(self, corr_len=1e-6):
+        corr1 = self.id_idx[0] - 1
+        corr2 = self.id_idx[-1] + 1
+
+        self.model[corr1].length += corr_len
+        self.model[corr2].length += corr_len
+        self.model[corr1 + 1].length -= corr_len
+        self.model[corr2 - 1].length -= corr_len
+
+        self.model[corr1].pass_method = 'str_mpole_symplectic4_pass'
+        self.model[corr2].pass_method = 'str_mpole_symplectic4_pass'
+        return [corr1, corr2]
+
+    def _calc_id_kickmat(self, corr_idx, dkick=1e-8):
+        mat = _np.zeros((2*self.bpm_idx.size, 2*len(corr_idx)))
+
+        for num, id_knb in enumerate(corr_idx):
+            # apply +delta/2
+            MeasIDIntegral.apply_id_deltakick(self.model, id_knb, dkick/2, 'x')
+            codp = pyaccel.tracking.find_orbit6(
+                self.model, indices=self.bpm_idx)
+            # apply -delta/2
+            MeasIDIntegral.apply_id_deltakick(self.model, id_knb, -dkick, 'x')
+            codn = pyaccel.tracking.find_orbit6(
+                self.model, indices=self.bpm_idx)
+            # recover model
+            MeasIDIntegral.apply_id_deltakick(self.model, id_knb, dkick/2, 'x')
+            diffcod = codp - codn
+            dcodxy = _np.hstack((diffcod[0, :], diffcod[2, :]))
+            mat[:, num] = dcodxy/dkick
+
+        for num, id_knb in enumerate(corr_idx):
+            # apply +delta/2
+            MeasIDIntegral.apply_id_deltakick(self.model, id_knb, dkick/2, 'y')
+            codp = pyaccel.tracking.find_orbit6(
+                self.model, indices=self.bpm_idx)
+            # apply -delta/2
+            MeasIDIntegral.apply_id_deltakick(self.model, id_knb, -dkick, 'y')
+            codn = pyaccel.tracking.find_orbit6(
+                self.model, indices=self.bpm_idx)
+            # recover model
+            MeasIDIntegral.apply_id_deltakick(self.model, id_knb, dkick/2, 'y')
+            diffcod = codp - codn
+            dcodxy = _np.hstack((diffcod[0, :], diffcod[2, :]))
+            mat[:, len(corr_idx) + num] = dcodxy/dkick
+        return mat
+
+    def fit_kicks(self, diff_orbit, idmat=None, nr_iter=5, nsv=None):
+        """."""
+        corr_idx = self._add_id_correctors()
+        if idmat is None:
+            idmat = self._calc_id_kickmat(corr_idx)
+        imat = MeasIDIntegral._svd_invert_matrix(idmat, nsv=nsv)
+        cod0 = pyaccel.tracking.find_orbit6(self.model, indices=self.bpm_idx)
+        cod0xy = _np.hstack((cod0[0, :], cod0[2, :]))
+        err = cod0xy - diff_orbit
+        print(
+            'initial error: {:.2f} um'.format(
+                MeasIDIntegral._calc_error(err)*1e6))
+
+        for _ in range(nr_iter):
+            dkick = _np.dot(imat, err)
+            MeasIDIntegral.apply_id_deltakick(
+                self.model, corr_idx[0], dkick[0], 'x')
+            MeasIDIntegral.apply_id_deltakick(
+                self.model, corr_idx[1], dkick[1], 'x')
+            MeasIDIntegral.apply_id_deltakick(
+                self.model, corr_idx[0], dkick[2], 'y')
+            MeasIDIntegral.apply_id_deltakick(
+                self.model, corr_idx[1], dkick[3], 'y')
+            cod = pyaccel.tracking.find_orbit6(
+                self.model, indices=self.bpm_idx)
+            codxy = _np.hstack((cod[0, :], cod[2, :]))
+            err = codxy - diff_orbit
+        print(
+            'final error: {:.2f} um'.format(
+                MeasIDIntegral._calc_error(err)*1e6))
+
+# static methods
+    @staticmethod
+    def _calc_error(err):
+        return _np.sqrt(_np.sum(err*err/err.size))
+
+    @staticmethod
+    def apply_id_deltakick(model, idx, dkick, plane):
+        """."""
+        if plane == 'x':
+            model[idx].hkick_polynom += dkick
+        elif plane == 'y':
+            model[idx].vkick_polynom += dkick
+        else:
+            raise ValueError('Plane must be x or y.')
+
+    @staticmethod
+    def _svd_invert_matrix(id_mat, nsv=None):
+        umat, smat, vhmat = _np.linalg.svd(id_mat, full_matrices=False)
+        ismat = 1/smat
+        ismat[_np.isnan(ismat)] = 0
+        ismat[_np.isinf(ismat)] = 0
+        if nsv is not None:
+            ismat[nsv:] = 0
+        ismat = _np.diag(ismat)
+        invmat = -1 * _np.dot(_np.dot(vhmat.T, ismat), umat.T)
+        return invmat
