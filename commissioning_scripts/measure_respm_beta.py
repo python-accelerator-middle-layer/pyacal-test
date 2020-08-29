a7bb9d82fbc18b59848f35246c261399af5b031e
Fernando <fernandohds564@gmail.com>
2020-08-29 18:09:02 -0300

    COMMIS.ENH: Add class to measure respmat.

--- commissioning_scripts/measure_respm_beta.py
+++ commissioning_scripts/measure_respm_beta.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python-sirius
 """."""
 
-import time
+import time as _time
 import pickle
+from threading import Thread as _Thread, Event as _Event
 
 import numpy as np
 
@@ -11,7 +12,8 @@ from siriuspy.devices import SOFB, Tune, CurrInfo, RFGen
 from siriuspy.clientconfigdb import ConfigDBClient
 
 from .base import BaseClass
-from .measure_beta import MeasBeta
+from .measure_beta import MeasBeta as _MeasBeta
+from .measure_respmat import MeasureRespMat as _MeasureRespMat
 
 
 class RespMatBetaParams:
@@ -21,143 +23,121 @@ class RespMatBetaParams:
         """."""
         self.nmeas_respmat = 10
         self.nmeas_beta = 10
+        self.filename = 'variation'
 
     def __str__(self):
         """."""
         dtmp = '{0:26s} = {1:9d}  {2:s}\n'.format
         stg = dtmp('nmeas_beta', self.nmeas_beta, '')
         stg += dtmp('nmeas_respmat', self.nmeas_respmat, '')
+        stg += f"filename = '{self.filename:s}'\n"
         return stg
 
 
 class MeasureRespMatBeta(BaseClass):
     """."""
+    _DEF_TIMEOUT = 60 * 60  # [s]
 
     def __init__(self):
         """."""
         super().__init__()
-        sofb, tune, curr, rfgen = self.create_devices()
-        self.devices['sofb'] = sofb
-        self.devices['tune'] = tune
-        self.devices['curr'] = curr
-        self.devices['rfgen'] = rfgen
         self.params = RespMatBetaParams()
-        self.measbeta = self._get_measbeta_object()
+        self.meas_beta = self._get_measbeta_object()
+        self.meas_respmat = _MeasureRespMat()
         self.confdb = ConfigDBClient(config_type='si_orbcorr_respm')
+        self._stopevt = _Event()
+        self._thread = _Thread(target=self.run_all, daemon=True)
 
-    @staticmethod
-    def create_devices():
+    def __str__(self):
         """."""
-        sofb = SOFB(SOFB.DEVICES.SI)
-        tune = Tune(Tune.DEVICES.SI)
-        curr = CurrInfo(CurrInfo.DEVICES.SI)
-        rfgen = RFGen(RFGen.DEVICES.AS)
-        return sofb, tune, curr, rfgen
+        stg = str(self.params)
+        stg += '\n\n MeasRespMat Params:\n\n'
+        stg += str(self.meas_respmat.params)
+        stg += '\n\n MeasBeta Params:\n\n'
+        stg += str(self.meas_beta.params)
+        return stg
 
-    def get_bpm_variation(self, period=10):
+    @property
+    def connected(self):
         """."""
-        sofb = self.devices['sofb']
-        orbx_pv = sofb.pv_object('SlowOrbX-Mon')
-        orby_pv = sofb.pv_object('SlowOrbY-Mon')
-        orbx_mon, orby_mon = [], []
+        conn = super().connected
+        conn &= self.meas_respmat.connected
+        conn &= self.meas_beta.connected
+        return conn
 
-        def orbx_cb(**kwargs):
-            nonlocal orbx_mon
-            orbx_mon.append(kwargs['value'])
-
-        def orby_cb(**kwargs):
-            nonlocal orby_mon
-            orby_mon.append(kwargs['value'])
-
-        orbx_pv.add_callback(orbx_cb)
-        orby_pv.add_callback(orby_cb)
-        time.sleep(period)
-        orbx_pv.clear_callbacks()
-        orby_pv.clear_callbacks()
-
-        orbx = np.array(orbx_mon)
-        orby = np.array(orby_mon)
-        orb_var = np.array([np.std(orbx, axis=0), np.std(orby, axis=0)])
-        return orb_var
-
-    def save_loco_setup(self, orbmat_name, fname):
+    def start(self):
         """."""
-        sofb = self.devices['sofb']
-        tune = self.devices['tune']
-        curr = self.devices['curr']
-        rfgen = self.devices['rfgen']
+        if self._thread.is_alive():
+            return
+        self._stopevt.clear()
+        self._thread = _Thread(target=self.run_all, daemon=True)
+        self._thread.start()
 
-        loco_setup = dict()
-        bpm_var = self.get_bpm_variation(period=10)
-        loco_setup['timestamp'] = time.time()
-        loco_setup['rf_frequency'] = rfgen.frequency
-        loco_setup['tunex'] = tune.tunex
-        loco_setup['tuney'] = tune.tuney
-        loco_setup['stored_current'] = curr.si.current
-        loco_setup['sofb_nr_points'] = sofb.nr_points
-        loco_setup['bpm_variation'] = bpm_var
-        loco_setup['orbmat_name'] = orbmat_name
-
-        if not fname.endswith('.pickle'):
-            fname += '.pickle'
-        with open(fname, 'wb') as fil:
-            pickle.dump(loco_setup, fil)
-
-    def correct_orbit(self):
+    def stop(self):
         """."""
-        sofb = self.devices['sofb']
-        wait_autocorr = 30
+        self._stopevt.set()
 
-        sofb.cmd_turn_on_autocorr()
-        time.sleep(wait_autocorr)
-        sofb.cmd_turn_off_autocorr()
-
-    def measure_respm(self):
+    @property
+    def ismeasuring(self):
         """."""
-        sofb = self.devices['sofb']
-        # Start the respm measurement
-        time.sleep(1)
-        sofb.cmd_measrespmat()
+        return self._thread.is_alive()
 
-        # Wait measurement to be finished
-        time.sleep(1)
-        sofb.wait_respm_meas()
-        time.sleep(1)
-        return sofb.respmat
-
-    def run_respmat(self):
+    def wait(self, timeout=None):
         """."""
-        for nms in range(self.params.nmeas_respmat):
-            print('Respmat Measurement Number {:d}'.format(nms+1))
-            orbmat_name = f'respmat_variation_n{nms+1:02d}'
-            setup_name = f'loco_setup_n{nms+1:02d}'
-            self.correct_orbit()
-            self.save_loco_setup(orbmat_name, setup_name)
-            respmat = self.measure_respm()
-            self.confdb.insert_config(orbmat_name, respmat)
-
-    def run_beta(self):
-        """."""
-        for nms in range(self.params.nmeas_beta):
-            print('Beta Measurement Number {:d}'.format(nms+1))
-            beta_name = f'beta_variation_n{nms+1:02d}'
-            self.correct_orbit()
-            self.measbeta = self._get_measbeta_object()
-            self.measbeta.start()
-            self.measbeta.wait()
-            self.measbeta.process_data(mode='pos')
-            self.measbeta.save_data(beta_name)
+        def_tim = MeasureRespMatBeta._DEF_TIMEOUT * (
+            self.params.nmeas_respmat, self.params.nmeas_beta)
+        timeout = timeout or def_tim
+        interval = 1  # [s]
+        ntrials = int(timeout/interval)
+        for _ in range(ntrials):
+            if not self.ismeasuring:
+                break
+            _time.sleep(interval)
+        else:
+            print('WARN: Timed out waiting beta measurement.')
 
     def run_all(self):
         """."""
         self.run_respmat()
         self.run_beta()
 
+    def run_respmat(self):
+        """."""
+        for nms in range(self.params.nmeas_respmat):
+            if self._stopevt.is_set():
+                break
+            print('Respmat Measurement Number {:d}'.format(nms+1))
+            respmat_name = f'{self.params.filename:s}_n{nms+1:02d}'
+            self.meas_respmat.params.respmat_name = respmat_name
+            self.meas_respmat.start()
+            self.meas_respmat.wait()
+            self.meas_respmat.save_data('respmat_' + respmat_name)
+
+    def run_beta(self):
+        """."""
+        for nms in range(self.params.nmeas_beta):
+            if self._stopevt.is_set():
+                break
+            print('Beta Measurement Number {:d}'.format(nms+1))
+            # correct orbit:
+            sofb = self.meas_respmat.devices['sofb']
+            sofb.correct_orbit_manually(self.meas_respmat.params.corr_nr_iters)
+
+            # start measurement:
+            beta_name = f'beta_{self.params.filename:s}_n{nms+1:02d}'
+            self.meas_beta = self._get_measbeta_object()
+            if self._stopevt.is_set():
+                break
+            self.meas_beta.start()
+            self.meas_beta.wait()
+            self.meas_beta.process_data(mode='pos')
+            self.meas_beta.save_data(beta_name)
+
     def _get_measbeta_object(self):
         """."""
         model = si.create_accelerator()
         famdata = si.get_family_data(model)
-        measbeta = MeasBeta(model, famdata)
+        measbeta = _MeasBeta(model, famdata)
         measbeta.params.nr_measures = 1
         measbeta.params.quad_deltakl = 0.01/2  # [1/m]
         measbeta.params.wait_quadrupole = 1  # [s]
