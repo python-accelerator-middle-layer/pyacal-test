0d6891ffaf070ae56536243fff92be28f07623ad
Fernando <fernandohds564@gmail.com>
2021-01-29 10:26:51 -0300

    COMMISS.ENH: Generalize class which varies PM delays.

--- commissioning_scripts/delay_maxkick_bopm.py
+++ commissioning_scripts/delay_maxkick_bopm.py
@@ -3,101 +3,112 @@
 import time as _time
 from threading import Thread as _Thread, Event as _Event
 
-import numpy as np
+import numpy as _np
 
-from mathphys.functions import save_pickle as _save_pickle, \
-    load_pickle as _load_pickle
 from siriuspy.namesys import SiriusPVName as _PVName
-from siriuspy.devices import PowerSupplyPU, Screen, BPM
+from siriuspy.devices import PowerSupplyPU, Screen, SOFB
+
+from .base import BaseClass
 
 
 class Params:
+    """."""
 
     def __init__(self):
-        self.delay_span = {'Sept': 3, 'Kckr': 2}  # delay in [us]
+        """."""
+        self.delay_span = 2  # delay in [us]
         self.num_points = 10
         self.wait_for_pm = 3
         self.num_mean_scrn = 20
         self.wait_mean_scrn = 0.5
 
     def __str__(self):
-        st = '{0:30s}= '.format('Delay Span [us]')
-        st += ',  '.join([
-            '{0:s}: {1:7.3f}'.format(k, v) for k, v in self.delay_span.items()
-            ]) + '\n'
-        st += '{0:30s}= {1:9.4f}\n'.format(
+        """."""
+        st = '{0:30s} = {1:7.3f}\n'.format('Delay Span [us]', self.delay_span)
+        st += '{0:30s} = {1:9.4f}\n'.format(
             'Scan Num. of points', self.num_points)
-        st += '{0:30s}= {1:9.4f}\n'.format(
+        st += '{0:30s} = {1:9.4f}\n'.format(
             'Mean Num. of points', self.num_mean_scrn)
-        st += '{0:30s}= {1:9.4f}\n'.format(
+        st += '{0:30s} = {1:9.4f}\n'.format(
             'Wait for PM [s]', self.wait_for_pm)
-        st += '{0:30s}= {1:9.4f}\n'.format(
+        st += '{0:30s} = {1:9.4f}\n'.format(
             'Wait Time [s]', self.wait_mean_scrn)
         return st
 
 
-class FindMaxPulsedMagnets:
+class FindBeamBasedPMDelays(BaseClass):
+    """."""
 
-    def __init__(self, pulsed_mags, screen, bpm):
-        dic = dict()
-        for mag in pulsed_mags:
-            mag = _PVName(mag)
-            dic[mag] = PowerSupplyPU(mag)
-        self.screen = Screen(screen)
-        self.bpm = BPM(bpm)
-        self._all_mags = dic
-        self._mags_to_measure = self.magnets
-        self._data_delay = {mag: [] for mag in dic}
-        self._data_bpmx = {mag: [] for mag in dic}
-        self._data_bpmy = {mag: [] for mag in dic}
-        self._data_x = {mag: [] for mag in dic}
-        self._data_y = {mag: [] for mag in dic}
-        self._data_sx = {mag: [] for mag in dic}
-        self._data_sy = {mag: [] for mag in dic}
-        self.params = Params()
+    def __init__(self, pulsed_mag, screen, sofb_acc='BO'):
+        """."""
+        super().__init__(params=Params())
+        self._magname = _PVName(pulsed_mag)
+        self._scrname = _PVName(screen)
+        self.devices[self._magname] = PowerSupplyPU(self._magname)
+        self.devices[screen] = Screen(self._scrname)
+        self.devices['sofb'] = SOFB(sofb_acc.upper() + '-Glob:AP-SOFB')
+        self.data = dict(
+            delay=[], trajx=[], trajy=[], posx=[], posy=[], sizex=[], sizey=[])
         self._thread = _Thread(target=self._findmax_thread)
         self._stopped = _Event()
 
     @property
-    def connected(self):
+    def magnet_name(self):
         """."""
-        conn = all([v.connected for v in self._all_mags.values()])
-        conn &= self.screen.connected
-        conn &= self.bpm.connected
-        return conn
+        return self._magname
 
     @property
-    def magnets(self):
+    def screen_name(self):
         """."""
-        return sorted(self._all_mags.keys())
+        return self._scrname
 
     @property
-    def magnets_to_measure(self):
+    def sofb(self):
         """."""
-        return self._mags_to_measure
+        return self.devices['sofb']
 
-    @magnets_to_measure.setter
-    def magnets_to_measure(self, mags):
+    @property
+    def screen(self):
         """."""
-        self._mags_to_measure = [_PVName(mag) for mag in mags if mag in self._all_mags]
+        return self.devices[self._scrname]
 
     @property
-    def datax(self):
-        return self._data_x
+    def magnet(self):
+        """."""
+        return self.devices[self._magname]
 
     @property
-    def datay(self):
-        return self._data_y
+    def trajx(self):
+        """."""
+        return self.data['trajx']
 
     @property
-    def datasx(self):
-        return self._data_sx
+    def trajy(self):
+        """."""
+        return self.data['trajy']
 
     @property
-    def datasy(self):
-        return self._data_sy
+    def posx(self):
+        """."""
+        return self.data['posx']
+
+    @property
+    def posy(self):
+        """."""
+        return self.data['posy']
+
+    @property
+    def sizex(self):
+        """."""
+        return self.data['sizex']
+
+    @property
+    def sizey(self):
+        """."""
+        return self.data['sizey']
 
     def start(self):
+        """."""
         if not self._thread.is_alive():
             self._thread = _Thread(
                 target=self._findmax_thread, daemon=True)
@@ -105,66 +116,48 @@ class FindMaxPulsedMagnets:
             self._thread.start()
 
     @property
-    def measuring(self):
+    def ismeasuring(self):
+        """."""
         return self._thread.is_alive()
 
     def stop(self):
+        """."""
         self._stopped.set()
 
-    def save_data(self, fname, overwrite=True):
-        """."""
-        data = {
-            'params': self.params,
-            'data_bpmx': self._data_bpmx,
-            'data_bpmy': self._data_bpmy,
-            'datax': self._data_x,
-            'datay': self._data_y,
-            'datasx': self._data_sx,
-            'datasy': self._data_sy,
-            'data_delay': self._data_delay}
-        _save_pickle(data, fname, overwrite=overwrite)
-
-    @staticmethod
-    def load_data(fname):
-        """."""
-        return _load_pickle(fname)
-
     def _findmax_thread(self):
-        corrs = self.magnets_to_measure
-        for cor in corrs:
-            print(cor)
-            mag = self._all_mags[cor]
-            delta = self.params.delay_span[
-                'Sept' if 'Sept' in cor.dev else 'Kckr']
-            origdelay = mag.delay
-            rangedelay = np.linspace(-delta/2, delta/2, self.params.num_points)
-            self._data_delay[cor] = []
-            self._data_bpmx[cor] = []
-            self._data_bpmy[cor] = []
-            self._data_x[cor] = []
-            self._data_y[cor] = []
-            self._data_sx[cor] = []
-            self._data_sy[cor] = []
-            for dly in rangedelay:
-                print('{0:9.4f}: Measuring'.format(origdelay+dly), end='')
-                mag.delay = origdelay + dly
-                _time.sleep(self.params.wait_for_pm)
-                for _ in range(self.params.num_mean_scrn):
-                    print('.', end='')
-                    self._data_delay[cor].append(mag.delay)
-                    self._data_bpmx[cor].append(self.bpm.sp_posx)
-                    self._data_bpmy[cor].append(self.bpm.sp_posy)
-                    self._data_x[cor].append(self.screen.centerx)
-                    self._data_y[cor].append(self.screen.centery)
-                    self._data_sx[cor].append(self.screen.sigmax)
-                    self._data_sy[cor].append(self.screen.sigmay)
-                    _time.sleep(self.params.wait_mean_scrn)
-                    if self._stopped.is_set():
-                        break
-                print('done!')
+        """."""
+        print(self._magname)
+        delta = self.params.delay_span
+
+        origdelay = self.magnet.delay
+        rangedelay = _np.linspace(-delta/2, delta/2, self.params.num_points)
+        self.data['delay'] = []
+        self.data['trajx'] = []
+        self.data['trajy'] = []
+        self.data['posx'] = []
+        self.data['posy'] = []
+        self.data['sizex'] = []
+        self.data['sizey'] = []
+        for dly in rangedelay:
+            print('{0:9.4f}: Measuring'.format(origdelay+dly), end='')
+            self.magnet.delay = origdelay + dly
+            _time.sleep(self.params.wait_for_pm)
+            for _ in range(self.params.num_mean_scrn):
+                print('.', end='')
                 if self._stopped.is_set():
                     break
-            mag.delay = origdelay
+                self.data['delay'].append(self.magnet.delay)
+                self.data['trajx'].append(self.sofb.trajx)
+                self.data['trajy'].append(self.sofb.trajy)
+                self.data['posx'].append(self.screen.centerx)
+                self.data['posy'].append(self.screen.centery)
+                self.data['sizex'].append(self.screen.sigmax)
+                self.data['sizey'].append(self.screen.sigmay)
+                _time.sleep(self.params.wait_mean_scrn)
+                if self._stopped.is_set():
+                    break
+            print('done!')
             if self._stopped.is_set():
                 break
+        self.magnet.delay = origdelay
         print('Finished!')
