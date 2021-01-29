0925c384b234dfed75ce9fe1b09c9e452edfa50c
murilobalves <alvesb.murilo@gmail.com>
2021-01-29 10:53:19 -0300

    OPTICS.ENH: Add LI quads in TB and code organization

--- commissioning_scripts/apply_nominal_optics.py
+++ commissioning_scripts/apply_nominal_optics.py
@@ -9,6 +9,9 @@ class SetOptics:
     """."""
 
     TB_QUADS = [
+        'LI-Fam:PS-QF2',
+        'LI-01:PS-QD2',
+        'LI-01:PS-QF3',
         'TB-01:PS-QD1',
         'TB-01:PS-QF1',
         'TB-02:PS-QD2A',
@@ -46,19 +49,9 @@ class SetOptics:
         self._select_magnets()
         self._create_devices()
         self.model = self.pymodpack.create_accelerator()
-
-        if optics_data is None:
-            optmode = self.optics_mode or self.pymodpack.default_optics_mode
-            self.optics_data = self.pymodpack.lattice.get_optics_mode(
-                optics_mode=optmode)
-            if 'T' in self.acc:
-                self.optics_data = self.optics_data[0]
-                self.model = self.model[0]
-
-        self.optics_data = dict(
-            (key.upper(), val) for key, val in self.optics_data.items())
+        self._create_optics_data()
         self.famdata = self.pymodpack.get_family_data(self.model)
-
+        # convert to integrated strengths
         for key in self.optics_data:
             if key in self.famdata:
                 idx = self.famdata[key]['index'][0][0]
@@ -94,32 +87,26 @@ class SetOptics:
             if mag not in self.devices:
                 self.devices[mag] = _PowerSupply(mag)
 
-    def get_applied_optics(self):
+    def _create_optics_data(self):
+        if self.optics_data is None:
+            optmode = self.optics_mode or self.pymodpack.default_optics_mode
+            self.optics_data = self.pymodpack.lattice.get_optics_mode(
+                optics_mode=optmode)
+            if 'T' in self.acc:
+                self.optics_data = self.optics_data[0]
+                self.model = self.model[0]
+
+        self.optics_data = dict(
+            (key.upper(), val) for key, val in self.optics_data.items())
+
+    def get_applied_strength(self):
         """."""
         for mag in self.devices:
             self.applied_optics[mag.dev] = self.devices[mag].strength
 
-    def compare_applied_model_optics(self):
-        """."""
-        for mag in self.devices:
-            diff = self.applied_optics[mag] - self.optics_data[mag.devname.dev]
-            diff /= self.optics_data[mag]
-            if 'Q' in mag:
-                print(
-                    '{0:30s}: KL_applied={1:9.4f}1/m, KL_model={2:9.4f}1/m, \
-                        diff={3:9.4f}%'.format(
-                        mag, self.applied_optics[mag],
-                        self.optics_data[mag.devname.dev], diff*100))
-            if 'S' in mag:
-                print(
-                    '{0:30s}: SL_applied={1:9.4f}1/m², SL_model={2:9.4f}1/m², \
-                        diff={3:9.4f}%'.format(
-                        mag, self.applied_optics[mag],
-                        self.optics_data[mag.devname.dev], diff*100))
-
     def _check_magtype(self, magtype):
+        mags = dict()
         if magtype == 'quadrupole':
-            mags = dict()
             for key in self.devices:
                 if 'Q' not in key:
                     continue
@@ -128,7 +115,6 @@ class SetOptics:
         elif magtype == 'sextupole':
             if 'T' in self.acc:
                 raise ValueError('transport lines do not have sextupoles')
-            mags = dict()
             for key in self.devices:
                 if 'S' not in key:
                     continue
@@ -142,25 +128,22 @@ class SetOptics:
             self, magtype, init=None, average=None, factor=0, apply=False):
         """."""
         mags = self._check_magtype(magtype)
-        init0 = []
-        goal = []
+        initv = []
+        goalv = []
         for mag in mags:
-            init0.append(mags[mag].strength)
-            goal.append(self.optics_data[mag.devname.dev])
-        init0 = _np.asarray(init0)
-        goal = _np.asarray(goal)
+            initv.append(mags[mag].strength)
+            magdev = mag.dev
+            if self.acc == 'TB' and 'LI' in mag:
+                mag.dev += 'L'
+            goalv.append(self.optics_data[magdev])
+        initv = _np.asarray(initv)
+        goalv = _np.asarray(goalv)
 
-        init = init0 if init is None else init
-        dperc = []
-        print(
-            '{:17s}  {:9s}  {:9s}  {:9s}%'.format(
-                '', 'applied', 'nominal', 'diff'))
-        for mag, stren in zip(mags, goal):
-            dperc.append((mags[mag].strength-stren)/stren*100)
-            print('{:17s}: {:9.6f}  {:9.6f}  {:9.6f}%'.format(
-                mag.devname, mags[mag].strength, stren, dperc[-1]))
+        init = initv if init is None else init
 
-        print()
+        dperc = SetOptics.print_current_status(
+            magnets=mags, goal_strength=goalv)
+
         average = _np.mean(dperc) if average is None else average
         print('average desired: {:+.4f} %'.format(average))
         print('average obtained: {:+.4f} %'.format(_np.mean(dperc)))
@@ -170,16 +153,38 @@ class SetOptics:
         dimp_perc = factor/100 * (-ddif)
         implem = (1 + dimp_perc/100) * init
 
-        print('-- to be implemented --')
-        print('factor: {:5.1f}%'.format(factor))
-        for mag, ini, imp in zip(mags, init, implem):
-            perc = (imp - ini) / ini * 100
-            print(
-                '{:17s}:  {:9.4f} -> {:9.4f}  [{:7.4}%]'.format(
-                    mags[mag].devname, ini, imp, perc))
+        SetOptics.print_strengths_implemented(
+            factor=factor, magnets=mags,
+            init_strength=init, implem_strength=implem)
 
         if apply:
             for mag, imp in zip(mags, implem):
                 mags[mag].strength = imp
             print('\n applied!')
         return init
+
+    @staticmethod
+    def print_current_status(magnets, goal_strength):
+        """."""
+        diff = []
+        print(
+            '{:17s}  {:9s}  {:9s}  {:9s}%'.format(
+                '', ' applied', ' goal', ' diff'))
+        for mag, stren in zip(magnets, goal_strength):
+            diff.append((magnets[mag].strength-stren)/stren*100)
+            print('{:17s}: {:9.6f}  {:9.6f}  {:9.6f}%'.format(
+                magnets[mag].devname, magnets[mag].strength, stren, diff[-1]))
+        print()
+        return diff
+
+    @staticmethod
+    def print_strengths_implemented(
+            factor, magnets, init_strength, implem_strength):
+        """."""
+        print('-- to be implemented --')
+        print('factor: {:5.1f}%'.format(factor))
+        for mag, ini, imp in zip(magnets, init_strength, implem_strength):
+            perc = (imp - ini) / ini * 100
+            print(
+                '{:17s}:  {:9.4f} -> {:9.4f}  [{:7.4}%]'.format(
+                    magnets[mag].devname, ini, imp, perc))
