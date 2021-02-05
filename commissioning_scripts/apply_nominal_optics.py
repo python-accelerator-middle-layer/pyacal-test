7c1d13e2f723e45cc3d6f16c69c31d2d0e1abfb2
murilobalves <alvesb.murilo@gmail.com>
2021-02-05 16:42:43 -0300

    OPTICS.ENH: Refactor apply_nominal optics

--- commissioning_scripts/apply_nominal_optics.py
+++ commissioning_scripts/apply_nominal_optics.py
@@ -26,7 +26,7 @@ class _Utils(_BaseClass):
         print()
         return diff
 
-    def _print_strengths_implemented(
+    def _print_strengths_to_be_implemented(
             self, factor, magnets, init_strength, implem_strength):
         """."""
         print('-- to be implemented --')
@@ -48,6 +48,16 @@ class _Utils(_BaseClass):
         for mag in devices_names:
             if mag not in self.devices:
                 self.devices[mag] = _PowerSupply(mag)
+
+    def _get_magnet_names(self, magtype='.*Q'):
+        regex = _re.compile(magtype)
+        mags = [mag for mag in self.devices if regex.match(mag)]
+        return mags
+
+    def _get_strengths(self, magtype):
+        mags = self._get_magnet_names(magtype)
+        stren = _np.asarray([self.devices[mag].strength for mag in mags])
+        return mags, stren
 
 
 class SetOpticsMode(_Utils):
@@ -80,24 +90,18 @@ class SetOpticsMode(_Utils):
         'TS-04:PS-QF4',
         ]
 
-    def __init__(self, acc, optics_mode=None, optics_data=None):
+    def __init__(self, acc, optics_mode=None):
         """Apply strengths of families to the machine for a given optics.
 
         Arguments:
         - acc: TB, BO, TS or SI.
         - optics_mode: available modes in pymodels. If None, default
         optics_mode for the accelerator will be used (optional).
-        - optics_data: dictionary with quadrupoles and sextupoles, if applies
-        for the accelerator, strengths not integrated (optional).
         """
         super().__init__()
         self.acc = acc.upper()
-        self.optics_mode = optics_mode
-        if optics_data is not None:
-            if not isinstance(optics_data, (dict, _OrderedDict)):
-                raise ValueError(
-                    'optics_data must be a dictionary or OrderedDict')
-        self.optics_data = optics_data
+        self.data = dict()
+        self.data['optics_mode'] = optics_mode
         self.model = None
         self.quad_list = []
         self.sext_list = []
@@ -110,10 +114,12 @@ class SetOpticsMode(_Utils):
         self._create_optics_data()
         self.famdata = self._pymodpack.get_family_data(self.model)
         # convert to integrated strengths
-        for key in self.optics_data:
+        for key in self.data['optics_data']:
             if key in self.famdata:
+                # NOTE: This is incorrect for magnets with more than one
+                # segment
                 idx = self.famdata[key]['index'][0][0]
-                self.optics_data[key] *= self.model[idx].length
+                self.data['optics_data'][key] *= self.model[idx].length
 
     def get_applied_strength(self, magnets=None):
         """."""
@@ -125,26 +131,22 @@ class SetOpticsMode(_Utils):
             self.applied_optics[mag.dev] = magnets[mag].strength
 
     def apply_strengths(
-            self, magtype, init=None, average=None, factor=0, apply=False):
+            self, magtype, goal_strength,
+            init_strength=None, average=None, factor=0, apply=False):
         """."""
-        mags = self._check_magtype(magtype)
-        initv = []
-        goalv = []
-        for mag in mags:
-            initv.append(mags[mag].strength)
-            magdev = mag.dev
-            if self.acc == 'TB' and 'LI' in mag:
-                magdev += 'L'
-            goalv.append(self.optics_data[magdev])
+        mags, initv = self._get_strengths(magtype)
         initv = _np.asarray(initv)
-        goalv = _np.asarray(goalv)
-        if init is not None:
-            if len(init) != len(mags):
+        goalv = _np.asarray(goal_strength)
+        if init_strength is not None:
+            if len(init_strength) != len(mags):
                 raise ValueError(
                     'initial strength vector length is incompatible with \
                     number of magnets')
-        init = initv if init is None else init
-
+        init = initv if init_strength is None else _np.asarray(init_strength)
+        if len(goal_strength) != len(mags):
+            raise ValueError(
+                'goal strength vector length is incompatible with \
+                number of magnets')
         dperc = self._print_current_status(magnets=mags, goal_strength=goalv)
         average = _np.mean(dperc) if average is None else average
         print(' average desired: {:+.4f} %'.format(average))
@@ -155,7 +157,7 @@ class SetOpticsMode(_Utils):
         dimp_perc = ddif * (factor/100)
         implem = (1 + dimp_perc/100) * init
 
-        self._print_strengths_implemented(
+        self._print_strengths_to_be_implemented(
             factor=factor, magnets=mags,
             init_strength=init, implem_strength=implem)
         if apply:
@@ -177,9 +179,9 @@ class SetOpticsMode(_Utils):
         slist = []
         pvstr = ''
         if self.acc == 'TB':
-            qlist = SetOpticsFamilies.TB_QUADS
+            qlist = SetOpticsMode.TB_QUADS
         elif self.acc == 'TS':
-            qlist = SetOpticsFamilies.TS_QUADS
+            qlist = SetOpticsMode.TS_QUADS
         else:
             pvstr = self.acc + '-Fam:PS-'
             qlist = self._pymodpack.families.families_quadrupoles()
@@ -188,20 +190,14 @@ class SetOpticsMode(_Utils):
         self.sext_list = [_PVName(pvstr+mag) for mag in slist]
 
     def _create_optics_data(self):
-        if self.optics_data is None:
-            optmode = self.optics_mode or self._pymodpack.default_optics_mode
-            self.optics_data = self._pymodpack.lattice.get_optics_mode(
-                optics_mode=optmode)
-            if 'T' in self.acc:
-                self.optics_data = self.optics_data[0]
-                self.model = self.model[0]
-        self.optics_data = _OrderedDict(
-            [(key.upper(), val) for key, val in self.optics_data.items()])
-
-    def _check_magtype(self, magtype='Q'):
-        regex = _re.REG(magtype)
-        mags = [mag for mag in self.devices if regex.match(mag)]
-        return mags
+        optmode = self.data['optics_mode']
+        if self.data['optics_mode'] is None:
+            optmode = self._pymodpack.default_optics_mode
+        self.data['optics_data'] = self._pymodpack.lattice.get_optics_mode(
+            optics_mode=optmode)
+        if 'T' in self.acc:
+            self.data['optics_data'] = self.data['optics_data'][0]
+            self.model = self.model[0]
 
 
 class SetCorretorsStrengths(_Utils):
@@ -213,19 +209,13 @@ class SetCorretorsStrengths(_Utils):
         self.acc = acc.upper()
         self._get_corr_names()
         self.devices = _OrderedDict()
-        self._create_devices(self.ch_list+self.cv_list)
-        self.applied_strength = _OrderedDict()
-
-    def get_applied_strength(self, magnets=None):
-        """."""
-        magnets = magnets or self.ch_list + self.cv_list
-        for mag in magnets:
-            self.applied_strength[mag] = self.devices[mag].strength
+        self._create_devices(
+            devices_names=self.ch_list+self.cv_list)
 
     def apply_factor(
             self, magtype=None, factor=1, apply=False):
         """."""
-        mags, init = self._get_initial_state(magtype)
+        mags, init = self._get_strengths(magtype)
         implem = factor * init
         print(
             'Factor {:9.3f} will be applied in kicks of {:10s} magnets'.format(
@@ -237,7 +227,7 @@ class SetCorretorsStrengths(_Utils):
     def change_average_kicks(
             self, magtype=None, average=None, percentage=5, apply=False):
         """."""
-        mags, init = self._get_initial_state(magtype)
+        mags, init = self._get_strengths(magtype)
         curr_ave = _np.mean(init)
         # If average is None, the applied average kicks will be unchanged
         goal_ave = curr_ave if average is None else average
@@ -253,7 +243,7 @@ class SetCorretorsStrengths(_Utils):
     def apply_delta_kicks(
             self, delta_kicks, magtype=None, percentage=5, apply=False):
         """."""
-        mags, init = self._get_initial_state(magtype)
+        mags, init = self._get_strengths(magtype)
         dkicks = _np.asarray(delta_kicks)
         if len(dkicks) != len(mags):
             raise ValueError(
@@ -266,7 +256,7 @@ class SetCorretorsStrengths(_Utils):
 
     def apply_kicks(self, kicks, magtype=None, percentage=5, apply=False):
         """."""
-        _, init = self._get_initial_state(magtype)
+        _, init = self._get_strengths(magtype)
         dkicks = kicks - init
         self.apply_delta_kicks(
             delta_kicks=dkicks, magtype=magtype,
@@ -280,21 +270,6 @@ class SetCorretorsStrengths(_Utils):
             {'sec': self.acc, 'dis': 'PS', 'dev': 'CV'})
         self.ch_list = [_PVName(mag) for mag in ch_names]
         self.cv_list = [_PVName(mag) for mag in cv_names]
-
-    def _check_magtype(self, magtype):
-        if magtype in ('CH', 'CV'):
-            mags = {
-                key: val for key, val in self.devices.items()
-                if magtype in key}
-            mags = _OrderedDict(mags)
-        else:
-            raise ValueError('magtype must be CH or CV.')
-        return mags
-
-    def _get_initial_state(self, magtype):
-        mags = self._check_magtype(magtype)
-        init = _np.asarray([mags[mag].strength for mag in mags])
-        return mags, init
 
 
 class SISetTrimStrengths(_Utils):
@@ -310,13 +285,14 @@ class SISetTrimStrengths(_Utils):
         self.skewquad_names = list()
         self._get_quad_names()
         self._get_skewquad_names()
-        self._create_devices(self.quad_names+self.skewquad_names)
+        self._create_devices(
+            devices_names=self.quad_names+self.skewquad_names)
 
     def apply_strengths(
             self, magtype, strengths, percentage=5,
             apply=False, print_change=False):
         """."""
-        mags, init = self._get_initial_state(magtype)
+        mags, init = self._get_strengths(magtype)
         stren = _np.asarray(strengths)
         if len(stren) != len(mags):
             raise ValueError(
@@ -332,7 +308,7 @@ class SISetTrimStrengths(_Utils):
             self, magtype, delta_strengths, percentage=5,
             apply=False, print_change=False):
         """."""
-        mags, init = self._get_initial_state(magtype)
+        mags, init = self._get_strengths(magtype)
         dstren = _np.asarray(delta_strengths)
         if len(dstren) != len(mags):
             raise ValueError(
@@ -345,7 +321,7 @@ class SISetTrimStrengths(_Utils):
             print()
             print('percentage of application: {:5.1f} %'.format(percentage))
             print()
-            self._print_strengths_implemented(
+            self._print_strengths_to_be_implemented(
                 factor=percentage, magnets=mags,
                 init_strength=init, implem_strength=implem)
         if apply:
@@ -356,7 +332,7 @@ class SISetTrimStrengths(_Utils):
             self, magtype, goal_model, ref_model=None,
             percentage=5, apply=False, print_change=True):
         """."""
-        mags, init = self._get_initial_state(magtype)
+        mags, init = self._get_strengths(magtype)
         refmod = ref_model or self.model
         cond = len(goal_model) != len(refmod)
         cond |= goal_model.length != refmod.length
@@ -392,7 +368,7 @@ class SISetTrimStrengths(_Utils):
             print()
             print('percentage of application: {:5.1f} %'.format(percentage))
             print()
-            self._print_strengths_implemented(
+            self._print_strengths_to_be_implemented(
                 factor=percentage, magnets=mags,
                 init_strength=init, implem_strength=implem)
         if apply:
@@ -428,17 +404,17 @@ class SISetTrimStrengths(_Utils):
             qname = f'SI-{sub}:PS-QS-{inst}'
             self.skewquad_names.append(qname.strip('-'))
 
-    def _get_initial_state(self, magtype):
-        mags = self._check_magtype(magtype)
-        init = _np.asarray([mags[mag].strength for mag in mags])
-        return mags, init
-
-    def _check_magtype(self, magtype):
+    def _get_magnet_names(self, magtype):
         if magtype.startswith('quad'):
             maglist = self.quad_names
         elif magtype.startswith('skew'):
             maglist = self.skewquad_names
         else:
-            maglist = [mag for mag in self.devices]
-
+            regex = _re.compile(magtype)
+            maglist = [mag for mag in self.devices if regex.match(mag)]
         return maglist
+
+    def _get_strengths(self, magtype):
+        mags = self._get_magnet_names(magtype)
+        stren = _np.asarray([self.devices[mag].strength for mag in mags])
+        return mags, stren
