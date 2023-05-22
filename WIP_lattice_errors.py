5ed49ea38b7095d713ee9b37e5a066e4454cf278
gabriel <gabriel.ascencao@lnls.br>
2023-05-22 18:16:32 -0300

    work in progress - changing structure of multipoles errors and fixing normal distribution

--- WIP_lattice_errors.py
+++ WIP_lattice_errors.py
@@ -5,14 +5,15 @@ import matplotlib.pyplot as _plt
 from mathphys.functions import save_pickle
 
 
-class config_errors:
+class ConfigErrors:
 
     def __init__(self):
         self._fam_names = []
         self._sigma_x = 0
         self._sigma_y = 0
         self._sigma_roll = 0
-        self._error_types = ['posx', 'posy', 'roll']
+        self._sigma_pitch = 0
+        self._sigma_yaw = 0
         self._sigmas = dict()
         self._um = 1e-6
         self._mrad = 1e-3
@@ -55,6 +56,22 @@ class config_errors:
         self._sigma_roll = value * self._mrad
 
     @property
+    def sigma_pitch(self):
+        return self._sigma_pitch
+
+    @sigma_pitch.setter
+    def sigma_pitch(self, value):
+        self._sigma_pitch = value * self._mrad
+
+    @property
+    def sigma_yaw(self):
+        return self._sigma_yaw
+
+    @sigma_yaw.setter
+    def sigma_yaw(self, value):
+        self._sigma_yaw = value * self._mrad
+
+    @property
     def sigmas(self):
         return self._sigmas
 
@@ -63,17 +80,16 @@ class config_errors:
         self._sigmas = value
 
 
-class dipoles_errors(config_errors):
+class MultipolesErrors(ConfigErrors):
 
     def __init__(self):
         super().__init__()
         self._sigma_excit = 0
-        self._sigma_kdip = 0
+        self._multipoles_dict = None
+        self._normal_multipoles_order = []
+        self._skew_multipoles_order = []
         self._sigma_multipoles_n = []
         self._sigma_multipoles_s = []
-        self._error_types = ['posx', 'posy', 'roll', 'excitation', 'kdip',
-                             'normal_multipoles', 'skew_multipoles']
-        self._set_default_dipole_config()
 
     @property
     def sigma_excit(self):
@@ -82,6 +98,65 @@ class dipoles_errors(config_errors):
     @sigma_excit.setter
     def sigma_excit(self, value):
         self._sigma_excit = value * self._percent
+
+    @property
+    def normal_multipoles_order(self):
+        return self._normal_multipoles_order
+
+    @normal_multipoles_order.setter
+    def normal_multipoles_order(self, value):
+        self._normal_multipoles_order = value
+
+    @property
+    def skew_multipoles_order(self):
+        return self._skew_multipoles_order
+
+    @skew_multipoles_order.setter
+    def skew_multipoles_order(self, value):
+        self._skew_multipoles_order = value
+
+    @property
+    def sigma_multipoles_n(self):
+        return self._sigma_multipoles_n
+
+    @sigma_multipoles_n.setter
+    def sigma_multipoles_n(self, value):
+        self._sigma_multipoles_n = value
+
+    @property
+    def sigma_multipoles_s(self):
+        return self._sigma_multipoles_s
+
+    @sigma_multipoles_s.setter
+    def sigma_multipoles_s(self, value):
+        self._sigma_multipoles_s = value
+
+    @property
+    def multipoles_dict(self):
+        return self._multipoles_dict
+
+    @multipoles_dict.setter
+    def multipoles_dict(self, value):
+        self._multipoles_dict = value
+
+    def create_multipoles_dict(self):
+        n_multipoles_order_dict = dict()
+        s_multipoles_order_dict = dict()
+        for i, order in enumerate(self.normal_multipoles_order):
+            n_multipoles_order_dict[order] = self.sigma_multipoles_n
+        for i, order in enumerate(self.skew_multipoles_order):
+            s_multipoles_order_dict[order] = self.sigma_multipoles_s
+        self.multipoles_dict = dict()
+        self.multipoles_dict['normal'] = n_multipoles_order_dict
+        self.multipoles_dict['skew'] = n_multipoles_order_dict
+
+
+class DipolesErrors(MultipolesErrors):
+
+    def __init__(self):
+        super().__init__()
+        self._sigma_kdip = 0
+        self._set_default_dipole_config()
 
     @property
     def sigma_kdip(self):
@@ -91,77 +166,38 @@ class dipoles_errors(config_errors):
     def sigma_kdip(self, value):
         self._sigma_kdip = value * self._percent
 
-    @property
-    def sigma_multipoles_n(self):
-        return self._sigma_multipoles_n
-
-    @sigma_multipoles_n.setter
-    def sigma_multipoles_n(self, value):
-        self._sigma_multipoles_n = value
-
-    @property
-    def sigma_multipoles_s(self):
-        return self._sigma_multipoles_s
-
-    @sigma_multipoles_s.setter
-    def sigma_multipoles_s(self, value):
-        self._sigma_multipoles_s = value
-
     def _set_default_dipole_config(self):
         self.fam_names = ['B1', 'B2', 'BC']
         self.sigma_x = 40
         self.sigma_y = 40
         self.sigma_roll = 0.30
+        self.sigma_pitch = 0.10
+        self.sigma_yaw = 0.10
         self.sigma_excit = 0.05
         self.sigma_kdip = 0.10
         self.sigma_multipoles_n = [0, 0, 0]
         self.sigma_multipoles_s = [0, 0, 0]
+        self.normal_multipoles_order = [2, 3, 4]
+        self.skew_multipoles_order = [2, 3, 4]
+        self.create_multipoles_dict()
 
         sigmas = dict()
         sigmas['posx'] = self.sigma_x
         sigmas['posy'] = self.sigma_y
         sigmas['roll'] = self.sigma_roll
+        sigmas['pitch'] = self.sigma_pitch
+        sigmas['yaw'] = self.sigma_yaw
         sigmas['excitation'] = self.sigma_excit
         sigmas['kdip'] = self.sigma_kdip
-        sigmas['normal_multipoles'] = self.sigma_multipoles_n
-        sigmas['skew_multipoles'] = self.sigma_multipoles_s
+        sigmas['multipoles'] = self.multipoles_dict
         self.sigmas = sigmas
 
 
-class quads_errors(config_errors):
+class QuadsErrors(MultipolesErrors):
 
     def __init__(self):
         super().__init__()
-        self._sigma_excit = 0
-        self._error_types = ['posx', 'posy', 'roll', 'excitation',
-                             'normal_multipoles', 'skew_multipoles']
-        self._sigma_multipoles_n = []
-        self._sigma_multipoles_s = []
         self._set_default_quad_config()
-
-    @property
-    def sigma_excit(self):
-        return self._sigma_excit
-
-    @sigma_excit.setter
-    def sigma_excit(self, value):
-        self._sigma_excit = value * self._percent
-
-    @property
-    def sigma_multipoles_n(self):
-        return self._sigma_multipoles_n
-
-    @sigma_multipoles_n.setter
-    def sigma_multipoles_n(self, value):
-        self._sigma_multipoles_n = value
-
-    @property
-    def sigma_multipoles_s(self):
-        return self._sigma_multipoles_s
-
-    @sigma_multipoles_s.setter
-    def sigma_multipoles_s(self, value):
-        self._sigma_multipoles_s = value
 
     def _set_default_quad_config(self):
         self.fam_names = ['QFA', 'QDA', 'Q1', 'Q2', 'Q3', 'Q4', 'QDB1',
@@ -169,54 +205,31 @@ class quads_errors(config_errors):
         self.sigma_x = 40
         self.sigma_y = 40
         self.sigma_roll = 0.30
+        self.sigma_pitch = 0.10
+        self.sigma_yaw = 0.10
         self.sigma_excit = 0.05
         self.sigma_multipoles_n = [0, 0, 0]
         self.sigma_multipoles_s = [0, 0, 0]
+        self.normal_multipoles_order = [3, 4, 5]
+        self.skew_multipoles_order = [3, 4, 5]
+        self.create_multipoles_dict()
 
         sigmas = dict()
         sigmas['posx'] = self.sigma_x
         sigmas['posy'] = self.sigma_y
         sigmas['roll'] = self.sigma_roll
+        sigmas['pitch'] = self.sigma_pitch
+        sigmas['yaw'] = self.sigma_yaw
         sigmas['excitation'] = self.sigma_excit
-        sigmas['normal_multipoles'] = self.sigma_multipoles_n
-        sigmas['skew_multipoles'] = self.sigma_multipoles_s
+        sigmas['multipoles'] = self.multipoles_dict
         self.sigmas = sigmas
 
 
-class sexts_errors(config_errors):
+class SextsErrors(MultipolesErrors):
 
     def __init__(self):
         super().__init__()
-        self._sigma_excit = 0
-        self._error_types = ['posx', 'posy', 'roll', 'excitation',
-                             'normal_multipoles', 'skew_multipoles']
-        self._sigma_multipoles_n = []
-        self._sigma_multipoles_s = []
         self._set_default_sext_config()
-
-    @property
-    def sigma_excit(self):
-        return self._sigma_excit
-
-    @sigma_excit.setter
-    def sigma_excit(self, value):
-        self._sigma_excit = value * self._percent
-
-    @property
-    def sigma_multipoles_n(self):
-        return self._sigma_multipoles_n
-
-    @sigma_multipoles_n.setter
-    def sigma_multipoles_n(self, value):
-        self._sigma_multipoles_n = value
-
-    @property
-    def sigma_multipoles_s(self):
-        return self._sigma_multipoles_s
-
-    @sigma_multipoles_s.setter
-    def sigma_multipoles_s(self, value):
-        self._sigma_multipoles_s = value
 
     def _set_default_sext_config(self):
         self.fam_names = ['SFA0', 'SDA0', 'SDA1', 'SFA1', 'SDA2', 'SDA3',
@@ -226,21 +239,27 @@ class sexts_errors(config_errors):
         self.sigma_x = 40
         self.sigma_y = 40
         self.sigma_roll = 0.17
+        self.sigma_pitch = 0.10
+        self.sigma_yaw = 0.10
         self.sigma_excit = 0.05
         self.sigma_multipoles_n = [0, 0, 0]
         self.sigma_multipoles_s = [0, 0, 0]
+        self.normal_multipoles_order = [4, 5, 6]
+        self.skew_multipoles_order = [4, 5, 6]
+        self.create_multipoles_dict()
 
         sigmas = dict()
         sigmas['posx'] = self.sigma_x
         sigmas['posy'] = self.sigma_y
         sigmas['roll'] = self.sigma_roll
+        sigmas['pitch'] = self.sigma_pitch
+        sigmas['yaw'] = self.sigma_yaw
         sigmas['excitation'] = self.sigma_excit
-        sigmas['normal_multipoles'] = self.sigma_multipoles_n
-        sigmas['skew_multipoles'] = self.sigma_multipoles_s
+        sigmas['multipoles'] = self.multipoles_dict
         self.sigmas = sigmas
 
 
-class girder_errors(config_errors):
+class GirderErrors(ConfigErrors):
 
     def __init__(self):
         super().__init__()
@@ -251,49 +270,64 @@ class girder_errors(config_errors):
         self.sigma_x = 80
         self.sigma_y = 80
         self.sigma_roll = 0.30
+        self.sigma_pitch = 0.10
+        self.sigma_yaw = 0.10
 
         sigmas = dict()
         sigmas['posx'] = self.sigma_x
         sigmas['posy'] = self.sigma_y
         sigmas['roll'] = self.sigma_roll
+        sigmas['pitch'] = self.sigma_pitch
+        sigmas['yaw'] = self.sigma_yaw
         self.sigmas = sigmas
 
 
-class bpms_errors(config_errors):
+class BpmsErrors(ConfigErrors):
 
     def __init__(self):
         super().__init__()
-        self._set_default_girder_config()
+        self._set_default_bpm_config()
 
-    def _set_default_girder_config(self):
+    def _set_default_bpm_config(self):
         self.fam_names = ['BPM']
         self.sigma_x = 20
         self.sigma_y = 20
         self.sigma_roll = 0.30
+        self.sigma_pitch = 0.10
+        self.sigma_yaw = 0.10
 
         sigmas = dict()
         sigmas['posx'] = self.sigma_x
         sigmas['posy'] = self.sigma_y
         sigmas['roll'] = self.sigma_roll
+        sigmas['pitch'] = self.sigma_pitch
+        sigmas['yaw'] = self.sigma_yaw
         self.sigmas = sigmas
 
 
-class manage_errors():
+class ManageErrors():
 
     @staticmethod
-    def generate_normal_dist(sigma, cutoff, dim, mean=0, seed=131071):
+    def generate_normal_dist(sigma, cutoff, dim, mean=0, seed=131070):
         _np.random.seed(seed=seed)
-        if not isinstance(sigma, list):
-            dist = _np.random.normal(loc=mean, scale=sigma, size=dim)
-            dist = _np.where(_np.abs(dist) < cutoff*sigma, dist, 0)
-        else:
-            dist = _np.ones((dim[0], dim[1], len(sigma)))
-            for i, sigma_ in enumerate(sigma):
-                dist[:, :, i] = _np.random.normal(
-                    loc=mean, scale=sigma_, size=dim)
-                dist[:, :, i] = _np.where(
-                    _np.abs(dist[:, :, i]) < cutoff*sigma_, dist[:, :, i], 0)
+        dist = _np.random.normal(loc=mean, scale=sigma, size=dim)
+        dist = _np.where(_np.abs(dist) < cutoff*sigma, dist, 0)
         return dist
+
+    # @staticmethod
+    # def generate_normal_dist(sigma, cutoff, dim, mean=0, seed=131070):
+    #     _np.random.seed(seed=seed)
+    #     if not isinstance(sigma, list):
+    #         dist = _np.random.normal(loc=mean, scale=sigma, size=dim)
+    #         dist = _np.where(_np.abs(dist) < cutoff*sigma, dist, 0)
+    #     else:
+    #         dist = _np.ones((dim[0], dim[1], len(sigma)))
+    #         for i, sigma_ in enumerate(sigma):
+    #             dist[:, :, i] = _np.random.normal(
+    #                 loc=mean, scale=sigma_, size=dim)
+    #             dist[:, :, i] = _np.where(
+    #                 _np.abs(dist[:, :, i]) < cutoff*sigma_, dist[:, :, i], 0)
+    #     return dist
 
     @staticmethod
     def generate_errors(error_configs, nr_mach, cutoff, fam_data, seed=131071):
@@ -302,9 +336,8 @@ class manage_errors():
             for fam_name in config.fam_names:
                 idcs = _np.array(fam_data[fam_name]['index'], dtype="object")
                 error_type_dict = dict()
-                for error_type in config.error_types:
-                    sigma = config.sigmas[error_type]
-                    error = manage_errors.generate_normal_dist(
+                for error_type, sigma in config.sigmas.items():
+                    error = ManageErrors.generate_normal_dist(
                         sigma=sigma, cutoff=cutoff,
                         dim=(nr_mach, len(idcs)), seed=seed)
                     error_type_dict[error_type] = error
