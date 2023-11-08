803c897ebd5704a2cae8ec31c73bfc4791798fd4
murilobalves <alvesb.murilo@gmail.com>
2023-11-08 17:05:09 -0300

    MNT: Use new save/load methods from mathphys to handle pickle and hdf5 files

--- utils.py
+++ utils.py
@@ -6,9 +6,7 @@ from copy import deepcopy as _dcopy
 
 from epics.ca import CAThread as _Thread
 
-from mathphys.functions import save_pickle as _save_pickle, \
-    load_pickle as _load_pickle, save_hdf5 as _save_hdf5, \
-    load_hdf5 as _load_hdf5
+from mathphys.functions import save as _save, load as _load
 
 import apsuite.commisslib as _commisslib
 
@@ -62,11 +60,7 @@ class DataBaseClass:
                 Defaults to False.
 
         """
-        kwgs = dict(overwrite=overwrite, compress=compress)
-        if fname.endswith(('.h5', '.hdf5', '.hdf', '.hd5')):
-            _save_hdf5(self.to_dict(), fname, **kwgs)
-        else:
-            _save_pickle(self.to_dict(), fname, **kwgs)
+        _save(self.to_dict(), fname, overwrite=overwrite, compress=compress)
 
     def load_and_apply(self, fname: str):
         """Load and apply `data` and `params` from pickle or HDF5 file.
@@ -74,7 +68,7 @@ class DataBaseClass:
         Args:
             fname (str): name of the pickle file. If extension is not provided,
                 '.pickle' will be added and a pickle file will be assumed.
-                If provided, must be '.pickle' for pickle files or
+                If provided, must be {'.pickle', '.pkl'} for pickle files or
                 {'.h5', '.hdf5', '.hdf', '.hd5'} for HDF5 files.
 
         """
@@ -82,27 +76,24 @@ class DataBaseClass:
 
     @staticmethod
     def load_data(fname: str):
-        """Load and return `data` and `params` from pickle of HDF5 file.
+        """Load and return `data` and `params` from pickle or HDF5 file.
 
         Args:
             fname (str): name of the pickle file. If extension is not provided,
                 '.pickle' will be added and a pickle file will be assumed.
-                If provided, must be '.pickle' for pickle files or
+                If provided, must be {'.pickle', '.pkl'} for pickle files or
                 {'.h5', '.hdf5', '.hdf', '.hd5'} for HDF5 files.
 
         Returns:
             data (dict): Dictionary with keys: `data` and `params`.
 
         """
-        if fname.endswith(('.h5', '.hdf5', '.hdf', '.hd5')):
-            return _load_hdf5(fname)
-        else:
-            try:
-                data = _load_pickle(fname)
-            except ModuleNotFoundError:
-                _sys.modules['apsuite.commissioning_scripts'] = _commisslib
-                data = _load_pickle(fname)
-            return data
+        try:
+            data = _load(fname)
+        except ModuleNotFoundError:
+            _sys.modules['apsuite.commissioning_scripts'] = _commisslib
+            data = _load(fname)
+        return data
 
 
 class ParamsBaseClass:
