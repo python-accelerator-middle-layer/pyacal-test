6235a5a878202a4a37951ccf4c69e30efdd08832
murilobalves <alvesb.murilo@gmail.com>
2024-06-25 15:53:12 -0300

    WIP: Include first devices version

---
+++ devices/famcms.py
@@ -0,0 +1,67 @@
+from .. import ALIAS_MAP
+
+from .base import DeviceSet
+from .power_supply import PowerSupply
+
+
+class FamCMs(DeviceSet):
+    """."""
+
+    PROPERTIES_DEFAULT = PowerSupply.PROPERTIES_DEFAULT
+
+    def __init__(
+        self, accelerator=DEFAULT_ACCELERATOR, cmnames=None, plane="HV"
+    ):
+        """."""
+        cmidcs = []
+        self.accelerator = accelerator
+        if cmnames is None:
+            cmnames = []
+            plane = plane.upper()
+            if "H" in plane:
+                hcmidcs, hcmnames = self._get_cm_names(
+                    devtype="Corrector Horizontal"
+                )
+                cmnames.extend(hcmnames)
+                cmidcs.extend(hcmidcs)
+            if "V" in plane.upper():
+                vcmidcs, vcmnames = self._get_cm_names(
+                    devtype="Corrector Vertical"
+                )
+                cmnames.extend(vcmnames)
+                cmidcs.extend(vcmidcs)
+
+        cmdevs = [PowerSupply(dev, auto_monitor_mon=False) for dev in cmnames]
+        super().__init__(cmdevs)
+        self._cm_names = cmnames
+        self._cm_idcs = cmidcs
+
+    def _get_cm_names(self, devtype):
+        cmidcs, cmnames = zip(
+            *sorted(
+                (amap["sim_info"]["indices"], alias)
+                for alias, amap in ALIAS_MAP.items()
+                if amap["accelerator"] == self.accelerator
+                and amap["cs_devtype"] == devtype
+            )
+        )
+        return list(cmidcs), list(cmnames)
+
+    @property
+    def cm_names(self):
+        """."""
+        return self._cm_names
+
+    @property
+    def cm_indices(self):
+        """."""
+        return self._cm_idcs
+
+    def get_currents(self):
+        """."""
+        return [cm.current for cm in self.devices]
+
+    def set_currents(self, currents):
+        """."""
+        for i, cm in enumerate(self.devices):
+            cm.current = currents[i]
---
+++ devices/power_supply.py
@@ -0,0 +1,43 @@
+from .. import ALIAS_MAP
+
+from .base import Device
+
+
+class PowerSupply(Device):
+    """."""
+
+    PROPERTIES_DEFAULT = (
+        "pwrstate_sp",
+        "pwrstate_rb",
+        "current_sp",
+        "current_rb",
+    )
+
+    def __init__(self, devname):
+        """."""
+        super().__init__(devname, props2init=PowerSupply.PROPERTIES_DEFAULT)
+
+        if "PowerSupply" not in ALIAS_MAP[devname]["info"]:
+            raise ValueError(
+                f"Device name: {devname} not valid for a PowerSupply."
+            )
+
+    @property
+    def pwrstate(self):
+        """."""
+        return self["pwrstate_sp"]
+
+    @pwrstate.setter
+    def pwrstate(self, value):
+        """."""
+        self["pwrstate_rb"] = value
+
+    @property
+    def current(self):
+        """."""
+        return self["current_rb"]
+
+    @current.setter
+    def current(self, value):
+        """."""
+        self["current_sp"] = value
